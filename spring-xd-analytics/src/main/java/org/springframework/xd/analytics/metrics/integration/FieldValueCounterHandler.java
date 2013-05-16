87dffcac801cf8f74a17e6316de72123e611b4b1
mpollack <mpollack@vmware.com>
2013-05-16 01:35:40 -0400

    XD-55 SI ServiceActivator for an XD Metrics backed Field Value Counter Initial implementation.

---
+++ spring-xd-analytics/src/main/java/org/springframework/xd/analytics/metrics/integration/FieldValueCounterHandler.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.xd.analytics.metrics.integration;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.integration.Message;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.util.Assert;
+import org.springframework.util.ObjectUtils;
+import org.springframework.xd.analytics.metrics.core.FieldValueCounterService;
+import org.springframework.xd.tuple.Tuple;
+
+/**
+ * Counts the occurrence of values for a set of JavaBean properties or Tuple fields using a FieldValueCounterService 
+ * 
+ * @author Mark Pollack
+ *
+ */
+public class FieldValueCounterHandler {
+
+	private final FieldValueCounterService fieldValueCounterService;
+
+	private Map<String, String> fieldNameToCounterNameMap;
+	
+	
+	public FieldValueCounterHandler(FieldValueCounterService fieldValueCounterService, String counterName, String fieldName) {
+		Assert.notNull(fieldValueCounterService, "FieldValueCounterService can not be null");
+		Assert.notNull(counterName, "counter Name can not be null");
+		Assert.hasText(fieldName, "Field name can not be null or empty string");
+		this.fieldValueCounterService = fieldValueCounterService;
+		fieldNameToCounterNameMap = new HashMap<String,String>();
+		fieldNameToCounterNameMap.put(fieldName, counterName);
+	}
+	
+	@ServiceActivator
+	public Message<?> process(Message<?> message) {
+		Object payload = message.getPayload();
+		if (payload instanceof Tuple) {
+			processTuple((Tuple)payload);
+		} else {
+			processPojo(payload);
+		}
+		return message;
+	}
+
+	private void processPojo(Object payload) {
+		BeanWrapper beanWrapper = new BeanWrapperImpl(payload);
+		for (Map.Entry<String, String> entry : this.fieldNameToCounterNameMap.entrySet()) {
+		    String fieldName = entry.getKey();
+		    String counterName = entry.getValue();			
+		    if (beanWrapper.isReadableProperty(fieldName)) {
+			  Object value = beanWrapper.getPropertyValue(fieldName);
+			  processValue(counterName, value);
+		    }
+		}
+	}
+
+
+	private void processTuple(Tuple tuple) {
+		for (Map.Entry<String, String> entry : this.fieldNameToCounterNameMap.entrySet()) {
+		    String fieldName = entry.getKey();
+		    String counterName = entry.getValue();	
+		    if (tuple.hasFieldName(fieldName)) {
+		    	Object value = tuple.getValue(fieldName);			
+		    	processValue(counterName, value);
+		    }
+		}
+	}
+	
+	
+	protected void processValue(String counterName, Object value) {
+		if ((value instanceof Collection) || ObjectUtils.isArray(value)) {			
+			Collection<?> c = (value instanceof Collection) ?
+					(Collection<?>) value : Arrays.asList(ObjectUtils.toObjectArray(value));
+			for (Object val : c) {
+				//TODO better conversion to a string
+				fieldValueCounterService.increment(counterName, val.toString());				
+			}
+		} else {
+			fieldValueCounterService.increment(counterName, value.toString());		
+		}
+	}
+}
---
+++ spring-xd-analytics/src/test/java/org/springframework/xd/analytics/metrics/integration/FieldValueCounterHandlerTests.java
@@ -0,0 +1,73 @@
+package org.springframework.xd.analytics.metrics.integration;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.Message;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.xd.analytics.metrics.common.ServicesConfig;
+import org.springframework.xd.analytics.metrics.core.FieldValueCounter;
+import org.springframework.xd.analytics.metrics.core.FieldValueCounterRepository;
+import org.springframework.xd.analytics.metrics.core.FieldValueCounterService;
+import org.springframework.xd.tuple.Tuple;
+import org.springframework.xd.tuple.TupleBuilder;
+
+@ContextConfiguration(classes=ServicesConfig.class, loader=AnnotationConfigContextLoader.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class FieldValueCounterHandlerTests {
+
+	@Autowired
+	private FieldValueCounterService fieldValueCounterService;
+	
+	@Autowired
+	private FieldValueCounterRepository repo;
+	
+	private final String mentionsFieldValueCounterName = "tweetMentionsCounter";
+	
+	@Before
+	@After
+	public void initAndCleanup() {
+		repo.delete(mentionsFieldValueCounterName);
+	}
+	
+	@Test
+	public void messageCountTest() {
+		FieldValueCounter counter = fieldValueCounterService.getOrCreate(mentionsFieldValueCounterName);
+				
+		assertThat(repo.findOne(counter.getName()), notNullValue());
+		
+		FieldValueCounterHandler handler = new FieldValueCounterHandler(fieldValueCounterService, mentionsFieldValueCounterName, "mentions");
+
+
+		Tuple tuple = TupleBuilder.tuple().of("mentions", Arrays.asList(new String[]{"markp","markf","jurgen"}));
+		Message<Tuple> message = MessageBuilder.withPayload(tuple).build();
+		
+		handler.process(message);		
+		Map<String, Double> counts = repo.findOne(mentionsFieldValueCounterName).getFieldValueCount();		
+		assertThat(counts.get("markp"), equalTo(1.0));
+		assertThat(counts.get("markf"), equalTo(1.0));
+		assertThat(counts.get("jurgen"), equalTo(1.0));
+		
+		
+		tuple = TupleBuilder.tuple().of("mentions", Arrays.asList(new String[]{"markp","jon","jurgen"}));
+		message = MessageBuilder.withPayload(tuple).build();
+		
+		handler.process(message);		
+		counts = repo.findOne(mentionsFieldValueCounterName).getFieldValueCount();		
+		assertThat(counts.get("markp"), equalTo(2.0));
+		assertThat(counts.get("jon"), equalTo(1.0));
+		assertThat(counts.get("jurgen"), equalTo(2.0));
+	}
+
+}

a38183c2170e4321a280a52e3f766ef1aa251078
Luke Taylor <luke.taylor@springsource.com>
2013-05-16 18:46:20 +0100

    XD-43: Tidy up MetricRepository Javadoc.
    May not need this at all now, but retaining it
    for the time being.

--- spring-xd-analytics/src/main/java/org/springframework/xd/analytics/metrics/core/MetricRepository.java
+++ spring-xd-analytics/src/main/java/org/springframework/xd/analytics/metrics/core/MetricRepository.java
@@ -1,17 +1,12 @@
 package org.springframework.xd.analytics.metrics.core;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
 
 /**
- * A repository to save, delete and find Metric instances.  Uses the Spring Data Repository
+ * A repository to save, delete and find Metric instances.  Uses the Spring Data CrudRepository
  * marker interface and conventions for method names and behavior.
  *
  * The name is the id and should be unique.
- *
- * Note: Will shift to inherit from CrudRepository
  *
  * @author Mark Pollack
  * @author Luke Taylor

003ab2763475828d5272ebe17e4fc13424987081
Gary Russell <grussell@gopivotal.com>
2013-06-08 07:33:23 +0100

    XD-213 Add ObjectToStringTransformer to Local Registry
    Compatibility with Redis mode.

--- spring-xd-dirt/src/main/java/org/springframework/integration/x/channel/registry/LocalChannelRegistry.java
+++ spring-xd-dirt/src/main/java/org/springframework/integration/x/channel/registry/LocalChannelRegistry.java
@@ -23,6 +23,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.integration.Message;
 import org.springframework.integration.MessageChannel;
 import org.springframework.integration.channel.AbstractMessageChannel;
 import org.springframework.integration.channel.DirectChannel;
@@ -30,6 +31,7 @@ import org.springframework.integration.channel.PublishSubscribeChannel;
 import org.springframework.integration.channel.interceptor.WireTap;
 import org.springframework.integration.core.SubscribableChannel;
 import org.springframework.integration.handler.BridgeHandler;
+import org.springframework.integration.transformer.ObjectToStringTransformer;
 import org.springframework.util.Assert;
 
 /**
@@ -187,7 +189,23 @@ public class LocalChannelRegistry implements ChannelRegistry, ApplicationContext
 	}
 
 	protected BridgeHandler bridge(SubscribableChannel from, MessageChannel to, String bridgeName) {
-		BridgeHandler handler = new BridgeHandler();
+		// Temporarily perform string conversion for compatibility with Redis Registry
+		BridgeHandler handler = new BridgeHandler() {
+
+			private final ObjectToStringTransformer objectToStringTransformer =
+					new ObjectToStringTransformer();
+
+			@Override
+			protected Object handleRequestMessage(Message<?> requestMessage) {
+				if (requestMessage.getPayload() instanceof String) {
+					return requestMessage;
+				}
+				else {
+					return this.objectToStringTransformer.transform(requestMessage);
+				}
+			}
+
+		};
 		handler.setOutputChannel(to);
 		handler.setBeanName(bridgeName);
 		handler.afterPropertiesSet();

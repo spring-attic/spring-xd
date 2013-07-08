5e5fc871503185a48ed2133fbc699ece7e447753
David Turanski <dturanski_at_vmware_dot_com>
2013-07-08 16:27:05 -0400

    Fixed gradle launch ./xd-admin problem

--- spring-xd-dirt/src/main/java/org/springframework/xd/dirt/rest/TapsController.java
+++ spring-xd-dirt/src/main/java/org/springframework/xd/dirt/rest/TapsController.java
@@ -16,25 +16,19 @@
 
 package org.springframework.xd.dirt.rest;
 
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.ExposesResourceFor;
-import org.springframework.hateoas.VndErrors.VndError;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.xd.dirt.module.ModuleDeploymentRequest;
-import org.springframework.xd.dirt.stream.EnhancedStreamParser;
 import org.springframework.xd.dirt.stream.TapDefinition;
 import org.springframework.xd.dirt.stream.TapDeployer;
 import org.springframework.xd.rest.client.domain.TapDefinitionResource;

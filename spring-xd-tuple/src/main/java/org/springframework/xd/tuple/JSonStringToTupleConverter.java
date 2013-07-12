e5a90a168b3fe2f7dea43791c866b929dbea8738
David Turanski <dturanski_at_vmware_dot_com>
2013-07-12 16:59:04 -0400

    XD-466 Add Json support to Tuple

---
+++ spring-xd-tuple/src/main/java/org/springframework/xd/tuple/JSonStringToTupleConverter.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.springframework.xd.tuple;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ * @author David Turanski
+ *
+ */
+public class JSonStringToTupleConverter implements Converter<String, Tuple> {
+	private final ObjectMapper mapper = new ObjectMapper();
+
+	@Override
+	public Tuple convert(String source) {
+		TupleBuilder builder = TupleBuilder.tuple();
+		try {
+
+			JsonNode root = mapper.readTree(source);
+			for (Iterator<Entry<String, JsonNode>> it = root.getFields(); it.hasNext();) {
+				Entry<String, JsonNode> entry = it.next();
+				String name = entry.getKey();
+				JsonNode node = entry.getValue();
+				if (node.isObject()) {
+					//tuple
+					builder.addEntry(name, convert(node.toString()));
+				} else if (node.isArray()) {
+					builder.addEntry(name, nodeToList(node));
+				} else {
+					if (name.equals("id")) {
+						//TODO how should this be handled?
+					} else if (name.equals("timestamp")) {
+						//TODO how should this be handled?
+					} else {
+						builder.addEntry(name, node.asText());
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return builder.build();
+	}
+
+	private List<Object> nodeToList(JsonNode node) {
+		List<Object> list = new ArrayList<Object>(node.size());
+		for (int i = 0; i < node.size(); i++) {
+			JsonNode item = node.get(i);
+			if (item.isObject()) {
+				//TODO
+			} else if (item.isArray()) {
+				list.add(nodeToList(item));
+			} else {
+				list.add(item.asText());
+			}
+		}
+		return list;
+	}
+
+}
---
+++ spring-xd-tuple/src/main/java/org/springframework/xd/tuple/TupleToJSonStringConverter.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.springframework.xd.tuple;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ObjectNode;
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ * @author David Turanski
+ *
+ */
+public class TupleToJSonStringConverter implements Converter<Tuple, String> {
+
+	private final ObjectMapper mapper = new ObjectMapper();
+
+	@Override
+	public String convert(Tuple source) {
+		ObjectNode root = toObjectNode(source);
+		String json = null;
+		try {
+			json = mapper.writeValueAsString(root);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return json;
+	}
+
+	private ObjectNode toObjectNode(Tuple source) {
+		ObjectNode root = mapper.createObjectNode();
+		root.put("id", source.getId().toString());
+		root.put("timestamp", source.getTimestamp());
+		for (int i = 0; i < source.size(); i++) {
+			Object value = source.getValues().get(i);
+			String name = source.getFieldNames().get(i);
+			if (value instanceof Tuple) {
+				root.put(name, toObjectNode((Tuple) value));
+			}
+			else if (!value.getClass().isPrimitive()) {
+				root.put(name,root.POJONode(value));
+			}
+			else {
+				root.put(name, value.toString());
+			}
+		}
+		return root;
+	}
+
+}

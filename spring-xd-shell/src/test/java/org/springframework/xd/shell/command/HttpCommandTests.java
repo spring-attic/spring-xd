a403b342c5d87949f4f2a2b92b477ea7555095e9
Gunnar Hillert <ghillert@gopivotal.com>
2013-08-07 18:15:24 -0400

    XD-626 Add Integration Tests

---
+++ spring-xd-shell/src/test/java/org/springframework/xd/shell/command/HttpCommandTests.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.xd.shell.command;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.springframework.integration.test.util.SocketUtils;
+
+/**
+ * Test http commands
+ *
+ * @author Gunnar Hillert
+ * @since 1.0
+ */
+public class HttpCommandTests extends AbstractStreamIntegrationTest {
+
+	private static final Log logger = LogFactory.getLog(HttpCommandTests.class);
+
+	@Rule
+	public TemporaryFolder testFolder = new TemporaryFolder();
+
+	/**
+	 * This test will create a stream using a HTTP source and a File Sink.
+	 * Subsequently the "http post" shell command is used to post a simple Ascii
+	 * String to the admin server.
+	 */
+	@Test
+	public void testHttpPostAsciiText() throws InterruptedException, IOException {
+
+		final int openPort = SocketUtils.findAvailableServerSocket(8000);
+		final File tempFile = testFolder.newFile("asciidata.txt");
+		final String tempFileName = tempFile.getName();
+		final String tempFileDir = tempFile.getParentFile().getAbsolutePath();
+
+		final String stringToPost = "hello";
+
+		final String streamName = "postAsciiData";
+		final String stream = String.format("http --port=%s | file --name=%s --dir=%s", openPort, tempFileName, tempFileDir);
+
+		logger.info("Creating Stream: " + stream);
+		stream().create(streamName, stream);
+
+		logger.info("Posting String: " + stringToPost);
+		getShell().executeCommand(String.format("http post --target http://localhost:%s --data \"%s\"", openPort, stringToPost));
+
+		final String resultData = FileUtils.readFileToString(tempFile);
+		assertEquals(stringToPost + "\n", resultData);
+	}
+
+	/**
+	 * This test will create a stream using a HTTP source and a File Sink.
+	 * Subsequently the "http post" shell command is used to post a UTF String
+	 * (Japanese) to the admin server.
+	 */
+	@Test
+	public void testHttpPostUtfText() throws InterruptedException, IOException {
+
+		final int openPort = SocketUtils.findAvailableServerSocket(8200);
+		final File tempFile = testFolder.newFile("utfdata.txt");
+		final String tempFileName = tempFile.getName();
+		final String tempFileDir = tempFile.getParentFile().getAbsolutePath();
+
+		/** I want to go to Japan. */
+		final String stringToPostInJapanese = "\u65e5\u672c\u306b\u884c\u304d\u305f\u3044\u3002";
+
+		final String streamName = "postUtf8Data";
+		final String stream = String.format("http --port=%s | file --name=%s --dir=%s", openPort, tempFileName, tempFileDir);
+
+		logger.info("Creating Stream: " + stream);
+		stream().create(streamName, stream);
+
+		logger.info("Posting String: " + stringToPostInJapanese);
+		getShell().executeCommand(String.format("http post --target http://localhost:%s --data \"%s\"", openPort, stringToPostInJapanese));
+
+		final String resultData = FileUtils.readFileToString(tempFile, "UTF-8");
+		assertEquals(StringEscapeUtils.unescapeJava(stringToPostInJapanese) + "\n", resultData);
+	}
+}

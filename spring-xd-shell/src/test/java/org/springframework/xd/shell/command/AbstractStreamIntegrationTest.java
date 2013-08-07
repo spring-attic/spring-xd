4f613ab10f2e60b4ae0310df6c1d2ddf0c85a530
ericbottard <eric.bottard+github@gmail.com>
2013-08-07 15:43:30 +0200

    XD-592: Add @Ignore'd tests that showcase the problem

--- spring-xd-shell/src/test/java/org/springframework/xd/shell/command/AbstractStreamIntegrationTest.java
+++ spring-xd-shell/src/test/java/org/springframework/xd/shell/command/AbstractStreamIntegrationTest.java
@@ -16,12 +16,19 @@
 
 package org.springframework.xd.shell.command;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.After;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.xd.shell.AbstractShellIntegrationTest;
 
 /**
- * Provides an @After JUnit lifecycle method that will destroy the definitions that were
- * created by calling executeXXXCreate methods.
+ * Provides an @After JUnit lifecycle method that will destroy the definitions that were created by calling
+ * executeXXXCreate methods.
  * 
  * @author Andy Clement
  * @author Mark Pollack
@@ -38,6 +45,8 @@ public abstract class AbstractStreamIntegrationTest extends AbstractShellIntegra
 	private AggregateCounterCommandTemplate aggOps;
 
 	private RichGaugeCommandTemplate richGaugeOps;
+
+	private Set<FileSink> fileSinks = new HashSet<AbstractStreamIntegrationTest.FileSink>();
 
 	public AbstractStreamIntegrationTest() {
 		streamOps = new StreamCommandTemplate(getShell());
@@ -74,6 +83,54 @@ public abstract class AbstractStreamIntegrationTest extends AbstractShellIntegra
 		counter().deleteDefaultCounter();
 		aggCounter().deleteDefaultCounter();
 		richGauge().deleteDefaultRichGauge();
+		cleanFileSinks();
+	}
+
+	private void cleanFileSinks() {
+		for (FileSink fileSink : fileSinks) {
+			fileSink.cleanup();
+		}
+	}
+
+	protected FileSink newFileSink() {
+		FileSink fileSink = new FileSink();
+		fileSinks.add(fileSink);
+		return fileSink;
+	}
+
+	protected static class FileSink {
+
+		private File file;
+
+		/**
+		 * Constructs a new File Sink with a generated temp file.
+		 */
+		public FileSink() {
+			try {
+				file = File.createTempFile("xd-test", "txt");
+			}
+			catch (IOException e) {
+				throw new IllegalStateException(e);
+			}
+		}
+
+		/**
+		 * Returns a representation of the sink suitable for inclusion in a stream definition, <i>e.g.</i> @code{file
+		 * --dir=xxxx --name=yyyy}
+		 */
+		@Override
+		public String toString() {
+			return String.format("file --dir=%s --name=%s", file.getParent(), file.getName());
+		}
+
+		public void cleanup() {
+			file.delete();
+		}
+
+		public String getContents() throws IOException {
+			FileReader fileReader = new FileReader(file);
+			return FileCopyUtils.copyToString(fileReader);
+		}
 	}
 
 }
--- spring-xd-shell/src/test/java/org/springframework/xd/shell/command/StreamCommandTests.java
+++ spring-xd-shell/src/test/java/org/springframework/xd/shell/command/StreamCommandTests.java
@@ -16,6 +16,8 @@
 
 package org.springframework.xd.shell.command;
 
+import java.io.IOException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Ignore;
@@ -111,14 +113,10 @@ public class StreamCommandTests extends AbstractStreamIntegrationTest {
 	 * assertThat("List equality", list1, equalTo(list2));
 	 */
 
-	// This test hangs the server (produces error: dispatcher has no subscribers for
-	// channel 'foox')
-	@Ignore
 	@Test
-	public void testNamedChannelSyntax() {
+	public void testNamedChannelWithNoConsumerShouldBuffer() {
 		logger.info("Create ticktock stream");
 		stream().create("ticktock-in", "http --port=9314 > :foox");
-		stream().create("ticktock-out", ":foo > log");
 		httpPostData("http://localhost:9314", "blahblah");
 	}
 
@@ -166,17 +164,41 @@ public class StreamCommandTests extends AbstractStreamIntegrationTest {
 		// TODO verify log outputs zzyyccxxbbaa
 	}
 
+	// See https://jira.springsource.org/browse/XD-592
 	@Ignore
 	@Test
-	public void testUsingLabels() {
+	public void testTappingAndChannels() {
+		stream().create("myhttp", "http --port=9314 | transform --expression=payload.toUpperCase() | log");
+
+		// Cureently fails with an infinite recursion loop in parser on the following line
+		stream().create("tap", "tap @myhttp.1 | log");
+		stream().create("tap_new", "tap myhttp.1 > log");
+		executeCommand("http post --data Dracarys! --target http://localhost:9314");
+		// TODO verify both logs output DRACARYS!
+	}
+
+	@Test
+	@Ignore
+	// See https://jira.springsource.org/browse/XD-592
+	public void testUsingLabels() throws IOException {
+		FileSink sink1 = newFileSink();
+		FileSink sink2 = newFileSink();
+		FileSink sink3 = newFileSink();
+
 		stream().create("myhttp", "http --port=9314 | flibble: transform --expression=payload.toUpperCase() | log");
-		// executeStreamCreate("wiretap","tap @myhttp.1 | transform --expression=payload.replaceAll('a','.') | log",true);
+		tap().create("wiretap1", "tap @myhttp.1 | transform --expression=payload.replaceAll('a','.') | %s", sink1);
+
 		// These variants of the above (which does work) don't appear to work although
 		// they do refer to the same source channel:
-		stream().create("wiretap", "tap myhttp.transform > transform --expression=payload.replaceAll('a','.') | log");
-		stream().create("wiretap", "tap myhttp.flibble > transform --expression=payload.replaceAll('a','.') | log");
+		tap().create("wiretap2", "tap myhttp.transform > transform --expression=payload.replaceAll('a','.') | %s",
+				sink2);
+		tap().create("wiretap3", "tap myhttp.flibble > transform --expression=payload.replaceAll('a','.') | %s", sink3);
 		httpPostData("http://localhost:9314", "Dracarys!");
 		// TODO verify both logs output DRACARYS!
+		assertEquals("DRACARYS!\n", sink1.getContents());
+		assertEquals("DRACARYS!\n", sink2.getContents());
+		assertEquals("DRACARYS!\n", sink3.getContents());
+
 	}
 
 }

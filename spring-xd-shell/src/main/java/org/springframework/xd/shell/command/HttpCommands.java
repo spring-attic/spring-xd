43fb2398c4d2462502ff76cff47afed482ada0d8
mpollack <mpollack@gopivotal.com>
2013-08-08 21:47:42 -0400

    XD-606 Document JSON quoting behavior in shell

--- spring-xd-shell/src/main/java/org/springframework/xd/shell/command/HttpCommands.java
+++ spring-xd-shell/src/main/java/org/springframework/xd/shell/command/HttpCommands.java
@@ -39,7 +39,7 @@ import org.springframework.web.client.RestTemplate;
 
 /**
  * Http commands.
- *
+ * 
  * @author Jon Brisbin
  * @author Ilayaperumal Gopinathan
  * @author Gunnar Hillert
@@ -52,16 +52,15 @@ public class HttpCommands implements CommandMarker {
 
 	@CliCommand(value = { POST_HTTPSOURCE }, help = "POST data to http endpoint")
 	public String postHttp(
-			@CliOption(mandatory = true, key = { "", "target" }, help = "the location to post to", unspecifiedDefaultValue = "http://localhost:9000")
-			String target, @CliOption(mandatory = true, key = "data", help = "the payload to post")
-			String data) {
+			@CliOption(mandatory = true, key = { "", "target" }, help = "the location to post to.  Note JSON fields should be separated by a comma without any spaces.", unspecifiedDefaultValue = "http://localhost:9000") String target,
+			@CliOption(mandatory = true, key = "data", help = "the payload to post") String data) {
 		final StringBuilder buffer = new StringBuilder();
 		URI requestURI = URI.create(target);
 		RestTemplate restTemplate = new RestTemplate();
 
 		final HttpHeaders headers = new HttpHeaders();
-		final MediaType mediaType = new MediaType(MediaType.TEXT_PLAIN.getType(),
-				MediaType.TEXT_PLAIN.getSubtype(), Charset.forName("UTF-8"));
+		final MediaType mediaType = new MediaType(MediaType.TEXT_PLAIN.getType(), MediaType.TEXT_PLAIN.getSubtype(),
+				Charset.forName("UTF-8"));
 		headers.setContentType(mediaType);
 		final HttpEntity<String> request = new HttpEntity<String>(data, headers);
 
@@ -93,17 +92,15 @@ public class HttpCommands implements CommandMarker {
 		}
 	}
 
-	private void outputRequest(String method, URI requestUri, MediaType mediaType, String requestData, StringBuilder buffer) {
-		buffer.append("> ").append(method)
-			.append(" (").append(mediaType.toString()).append(") ")
-			.append(requestUri.toString())
-			.append(" ").append(requestData)
-				.append(OsUtils.LINE_SEPARATOR);
+	private void outputRequest(String method, URI requestUri, MediaType mediaType, String requestData,
+			StringBuilder buffer) {
+		buffer.append("> ").append(method).append(" (").append(mediaType.toString()).append(") ").append(
+				requestUri.toString()).append(" ").append(requestData).append(OsUtils.LINE_SEPARATOR);
 	}
 
 	private void outputResponse(ResponseEntity<String> response, StringBuilder buffer) {
-		buffer.append("> ").append(response.getStatusCode().value()).append(" ")
-				.append(response.getStatusCode().name()).append(OsUtils.LINE_SEPARATOR);
+		buffer.append("> ").append(response.getStatusCode().value()).append(" ").append(response.getStatusCode().name()).append(
+				OsUtils.LINE_SEPARATOR);
 		for (Map.Entry<String, List<String>> entry : response.getHeaders().entrySet()) {
 			buffer.append("> ").append(entry.getKey()).append(": ");
 			boolean first = true;

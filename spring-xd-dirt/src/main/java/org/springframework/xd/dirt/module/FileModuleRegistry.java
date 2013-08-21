0b1bcc6c9beeb061dc401f34353caf4cba16ae5c
Eric Bottard <eric.bottard+github@gmail.com>
2013-08-21 15:03:00 +0200

    XD-718 Use ParentLastClassLoader for Modules
    XD-720 Add test for module with classpath
    
    Add javadoc
    
    Introduce classpath concept for modules
    
    Actually use a custom ClassPath
    
    Addressed review comments
    
    Add module type in test

--- spring-xd-dirt/src/main/java/org/springframework/xd/dirt/module/FileModuleRegistry.java
+++ spring-xd-dirt/src/main/java/org/springframework/xd/dirt/module/FileModuleRegistry.java
@@ -17,22 +17,36 @@
 package org.springframework.xd.dirt.module;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.net.URL;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.util.Assert;
-import org.springframework.xd.module.ModuleDefinition;
-import org.springframework.xd.module.ModuleType;
 
 /**
+ * File based implementation of {@link ModuleRegistry} that supports two kinds of modules:
+ * <ul>
+ * <li>the "simple" case is a sole xml file, located in a directory named after the module type, <i>e.g.</i>
+ * {@code source/time.xml}</li>
+ * <li>the "enhanced" case is made up of a directory, where the application context file lives in a config sub-directory
+ * <i>e.g.</i> {@code source/time/config/time.xml} and extra classpath is loaded from jars in a lib subdirectory
+ * <i>e.g.</i> {@code source/time/lib/*.jar}</li>
+ * </ul>
+ * 
  * @author Mark Fisher
  * @author Glenn Renfro
+ * @author Eric Bottard
  */
-public class FileModuleRegistry extends AbstractModuleRegistry {
+public class FileModuleRegistry extends AbstractModuleRegistry implements ApplicationContextAware {
 
 	private final File directory;
+
+	private ResourcePatternResolver resolver;
 
 	public FileModuleRegistry(String directory) {
 		File f = new File(directory);
@@ -41,8 +55,14 @@ public class FileModuleRegistry extends AbstractModuleRegistry {
 	}
 
 	@Override
-	protected Resource loadResource(String name, String type) {
-		File file = new File(directory, type + File.separator + name + ".xml");
+	protected Resource locateApplicationContext(String name, String type) {
+		File typedDir = new File(directory, type);
+
+		File enhanced = new File(typedDir, name + File.separator + "config" + File.separator + name + ".xml");
+		if (enhanced.exists()) {
+			return new FileSystemResource(enhanced);
+		}
+		File file = new File(typedDir, name + ".xml");
 		if (file.exists()) {
 			return new FileSystemResource(file);
 		}
@@ -50,15 +70,29 @@ public class FileModuleRegistry extends AbstractModuleRegistry {
 	}
 
 	@Override
-	public List<ModuleDefinition> findDefinitions(String name) {
-		ArrayList<ModuleDefinition> definitions = new ArrayList<ModuleDefinition>();
-		for (ModuleType type : ModuleType.values()) {
-			Resource resource = loadResource(name, type.getTypeName());
-			if (resource != null) {
-				ModuleDefinition moduleDef = new ModuleDefinition(name, type.getTypeName(), resource);
-				definitions.add(moduleDef);
+	protected URL[] maybeLocateClasspath(Resource resource, String name, String type) {
+		try {
+			URL resourceLocation = resource.getURL();
+			if (resourceLocation.toString().endsWith(name + "/config/" + name + ".xml")) {
+				Resource jarsPattern = resource.createRelative("../lib/*.jar");
+				Resource[] jarsResources = resolver.getResources(jarsPattern.getURI().toString());
+				URL[] result = new URL[jarsResources.length];
+				for (int i = 0; i < jarsResources.length; i++) {
+					result[i] = jarsResources[i].getURL();
+				}
+				return result;
 			}
 		}
-		return definitions;
+		catch (IOException e) {
+			// Can't happen as we know resource is a FileSystemResource
+			throw new IllegalStateException(e);
+		}
+		return null;
 	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		resolver = applicationContext;
+	}
+
 }

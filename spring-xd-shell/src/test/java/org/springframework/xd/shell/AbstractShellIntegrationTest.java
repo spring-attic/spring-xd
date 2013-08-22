cf847ced49bf6c103696866a04f38e001a06087f
Eric Bottard <eric.bottard+github@gmail.com>
2013-08-22 10:15:17 +0200

    XD-720 Add test for module with classpath

--- spring-xd-shell/src/test/java/org/springframework/xd/shell/AbstractShellIntegrationTest.java
+++ spring-xd-shell/src/test/java/org/springframework/xd/shell/AbstractShellIntegrationTest.java
@@ -18,11 +18,17 @@ package org.springframework.xd.shell;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 
@@ -50,6 +56,16 @@ import org.springframework.xd.test.redis.RedisAvailableRule;
  */
 public abstract class AbstractShellIntegrationTest {
 
+	/**
+	 * Where test module definition assets reside, relative to this project cwd.
+	 */
+	private static final File TEST_MODULES_SOURCE = new File("src/test/resources/spring-xd/xd/modules/");
+
+	/**
+	 * Where test modules should end up, relative to this project cwd.
+	 */
+	private static final File TEST_MODULES_TARGET = new File("../modules/");
+
 	// These two are used across the tests, hopefully 9193 is free on most dev boxes and
 	// CI servers
 	public static final String DEFAULT_HTTP_PORT = "9193";
@@ -66,6 +82,8 @@ public abstract class AbstractShellIntegrationTest {
 	private static SingleNodeServer server;
 
 	private static JLineShellComponent shell;
+
+	private Set<File> toBeDeleted = new HashSet<File>();
 
 	@BeforeClass
 	public static void startUp() throws InterruptedException, IOException {
@@ -113,6 +131,38 @@ public abstract class AbstractShellIntegrationTest {
 	 */
 	protected void httpPostData(String target, String data) {
 		executeCommand("http post --target " + target + " --data " + data);
+	}
+
+	/**
+	 * Copies over module files (including jars if this is a directory-style module) from src/test/resources to where it
+	 * will be picked up and makes sure it will disappear at test end.
+	 * 
+	 * @param type the type of module, e.g. "source"
+	 * @param name the module name, with extension (e.g. time2.xml or time2 if a directory)
+	 * @throws IOException
+	 */
+	protected void installTestModule(String type, String name) throws IOException {
+		File toCopy = new File(TEST_MODULES_SOURCE, type + File.separator + name);
+		File destination = new File(TEST_MODULES_TARGET, type + File.separator + name);
+		Assert.assertFalse(
+				String.format("Destination %s already present. Make sure you're not overwriting a "
+						+ "standard module, or if this is from a previous aborted test run, please delete manually",
+						destination),
+				destination.exists());
+		toBeDeleted.add(destination);
+		if (toCopy.isDirectory()) {
+			FileUtils.copyDirectory(toCopy, destination);
+		}
+		else {
+			FileUtils.copyFile(toCopy, destination);
+		}
+	}
+
+	@After
+	public void cleanTestModuleFiles() {
+		for (File file : toBeDeleted) {
+			FileUtils.deleteQuietly(file);
+		}
 	}
 
 }

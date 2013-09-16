30be879c323d0445b64bbb52acb3c4f9a0ad16d7
Eric Bottard <eric.bottard+github@gmail.com>
2013-09-16 13:34:52 +0200

    Add MaxPermGen option to gradle wrapper

--- gradlew.bat
+++ gradlew.bat
@@ -1,11 +1,14 @@
 @if "%DEBUG%" == "" @echo off
 SETLOCAL
+
+set GRADLE_OPTS="-XX:MaxPermSize=256m %GRADLE_OPTS%"
+
 if not "%XD_HOME%" == "" (
 	set XD_HOME=..
 )
 if not "%XD_TRANSPORT%" == "" (
 	set XD_TRANSPORT=local
-) 
+)
 if not "%XD_ANALYTICS%" == "" (
 	set XD_ANALYTICS=memory
 )

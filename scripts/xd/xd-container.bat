2e53987ff1ff7a556acb4ddadcfcb0c8937cd1bd
Thomas Risberg <trisberg@gopivotal.com>
2013-09-18 18:35:07 -0400

    XD-847 Revise the available hadoopDistro options
    Available options now are hadoop12 (default), cdh4, hdp13, phd1, hadoop20

--- scripts/xd/xd-container.bat
+++ scripts/xd/xd-container.bat
@@ -72,7 +72,7 @@ set CMD_LINE_ARGS=%$
 
 @echo off
 set APP_HOME_LIB=%APP_HOME%\lib
-set HADOOP_DISTRO=hadoop10
+set HADOOP_DISTRO=hadoop12
 if exist "%APP_HOME_LIB%" (
     setLocal EnableDelayedExpansion
     set found=0

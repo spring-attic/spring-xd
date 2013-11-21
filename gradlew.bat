4272bd6883066aa6353fddc1f878a5ec5775eff9
David Turanski <dturanski_at_gopivotal_dot_com>
2013-11-21 14:43:18 -0500

    unset XD_* in gradlew

--- gradlew.bat
+++ gradlew.bat
@@ -4,16 +4,16 @@ SETLOCAL
 set GRADLE_OPTS="-XX:MaxPermSize=256m %GRADLE_OPTS%"
 
 if not "%XD_HOME%" == "" (
-	set XD_HOME=..
+	set XD_HOME=
 )
 if not "%XD_TRANSPORT%" == "" (
-	set XD_TRANSPORT=local
+	set XD_TRANSPORT=
 )
 if not "%XD_ANALYTICS%" == "" (
-	set XD_ANALYTICS=memory
+	set XD_ANALYTICS=
 )
 if not "%XD_STORE%" == "" (
-	set XD_STORE=memory
+	set XD_STORE=
 )
 call gradle\build_xd.bat  %*
 

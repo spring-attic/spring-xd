1475626571700462348eb1a4153dfec526c4a607
Dave Syer <dsyer@gopivotal.com>
2013-11-26 15:27:02 +0000

    XD-1128: Add JMX support to parent contexts
    Also rely implicitly on Spring Boot to set up the Jolokia
    endpoints. Example:
    
    http://localhost:9393/jolokia/search/xd.*:type=*,*

--- spring-xd-dirt/src/main/java/org/springframework/xd/dirt/server/LauncherApplication.java
+++ spring-xd-dirt/src/main/java/org/springframework/xd/dirt/server/LauncherApplication.java
@@ -2,14 +2,17 @@
 package org.springframework.xd.dirt.server;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.event.SourceFilteringListener;
+import org.springframework.integration.monitor.IntegrationMBeanExporter;
 import org.springframework.xd.dirt.container.ContainerStartedEvent;
 import org.springframework.xd.dirt.container.XDContainer;
 import org.springframework.xd.dirt.server.options.ContainerOptions;
@@ -57,4 +60,17 @@ public class LauncherApplication {
 		delegate.setEnvironment(context.getEnvironment());
 		return new SourceFilteringListener(context, delegate);
 	}
+
+	@ConditionalOnExpression("${XD_JMX_ENABLED:false}")
+	@EnableMBeanExport(defaultDomain = "xd.container")
+	protected static class JmxConfiguration {
+
+		@Bean
+		public IntegrationMBeanExporter integrationMBeanExporter() {
+			IntegrationMBeanExporter exporter = new IntegrationMBeanExporter();
+			exporter.setDefaultDomain("xd.container");
+			return exporter;
+		}
+	}
+
 }

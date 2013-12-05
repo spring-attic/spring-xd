ae34e5e867557738df8c9dc1ddeca95e4521f598
David Turanski <dturanski_at_gopivotal_dot_com>
2013-12-05 10:47:24 -0500

    Gemfire server enhancements

--- spring-xd-gemfire-server/src/main/java/org/springframework/xd/gemfire/CacheServer.java
+++ spring-xd-gemfire-server/src/main/java/org/springframework/xd/gemfire/CacheServer.java
@@ -13,10 +13,18 @@
 
 package org.springframework.xd.gemfire;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.gemstone.gemfire.cache.Cache;
+import com.gemstone.gemfire.management.CacheServerMXBean;
+import com.gemstone.gemfire.management.ManagementService;
 
 /**
  * @author David Turanski
@@ -26,15 +34,43 @@ public class CacheServer {
 
 	private static final Log logger = LogFactory.getLog(CacheServer.class);
 
+	private static final int port = 40404;
+
 	public static void main(String[] args) {
 		if (args.length != 1) {
 			System.out.println("Usage: CacheServer <config-file-path>");
 			System.exit(1);
 		}
 		String path = args[0];
+
+		if (!portAvailable(port)) {
+			System.out.println("Cache Server port " + port + " is not available. Is another instance running?");
+			System.exit(1);
+		}
 		logger.info("Starting Cache Server");
 		@SuppressWarnings("resource")
 		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext(path);
 		context.registerShutdownHook();
+		Cache cache = context.getBean(Cache.class);
+		ManagementService ms = ManagementService.getExistingManagementService(cache);
+		CacheServerMXBean cacheServerManager = ms.getLocalCacheServerMXBean(port);
+		if (!cacheServerManager.isRunning()) {
+			System.out.println("failed to start cache server ");
+			System.exit(1);
+		}
+	}
+
+	public static boolean portAvailable(int port) {
+		InetSocketAddress addr = new InetSocketAddress(port);
+		try {
+			ServerSocket socket = new ServerSocket();
+			socket.bind(addr);
+			socket.close();
+		}
+		catch (IOException e) {
+			return false;
+		}
+		return true;
+
 	}
 }

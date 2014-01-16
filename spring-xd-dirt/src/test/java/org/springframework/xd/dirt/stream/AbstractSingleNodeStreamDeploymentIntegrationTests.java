155572620cefc5cee11ac23481bc5e2d2b1d8765
David Turanski <dturanski_at_gopivotal_dot_com>
2014-01-16 14:56:30 -0500

    XD-1247 XD-1248. Cleanup AbstractSingleNodeStreamDeploymentIntegrationTests

--- spring-xd-dirt/src/test/java/org/springframework/xd/dirt/stream/AbstractSingleNodeStreamDeploymentIntegrationTests.java
+++ spring-xd-dirt/src/test/java/org/springframework/xd/dirt/stream/AbstractSingleNodeStreamDeploymentIntegrationTests.java
@@ -14,19 +14,37 @@
 package org.springframework.xd.dirt.stream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.http.MediaType;
+import org.springframework.integration.channel.AbstractMessageChannel;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.channel.interceptor.WireTap;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.integration.x.bus.MessageBus;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
+import org.springframework.xd.dirt.event.AbstractModuleEvent;
+import org.springframework.xd.dirt.server.SingleNodeApplication;
+import org.springframework.xd.module.ModuleDefinition;
 import org.springframework.xd.module.core.Module;
 
 
@@ -39,13 +57,21 @@ import org.springframework.xd.module.core.Module;
  * @author Gunnar Hillert
  * @author Mark Fisher
  */
-public abstract class AbstractSingleNodeStreamDeploymentIntegrationTests extends
-		AbstractStreamDeploymentIntegrationTests {
+public abstract class AbstractSingleNodeStreamDeploymentIntegrationTests {
 
+	private AbstractApplicationContext context;
 
-	@Override
-	protected void cleanup(ApplicationContext context) {
-	}
+	private SingleNodeApplication application;
+
+	protected StreamDefinitionRepository streamDefinitionRepository;
+
+	protected StreamRepository streamRepository;
+
+	protected StreamDeployer streamDeployer;
+
+	private ModuleEventListener moduleEventListener = new ModuleEventListener();
+
+	private final QueueChannel tapChannel = new QueueChannel();
 
 	@Test
 	public final void testRoutingWithSpel() throws InterruptedException {
@@ -103,6 +129,101 @@ public abstract class AbstractSingleNodeStreamDeploymentIntegrationTests extends
 
 	}
 
+
+	@Before
+	public final void setUp() {
+		String transport = this.getTransport();
+		this.application = new SingleNodeApplication();
+		application.run("--transport", transport);
+
+		this.context = (AbstractApplicationContext) application.getContainerContext();
+		this.streamDefinitionRepository = context.getBean(StreamDefinitionRepository.class);
+		this.streamRepository = context.getBean(StreamRepository.class);
+		this.streamDeployer = application.getAdminContext().getBean(StreamDeployer.class);
+
+		AbstractMessageChannel deployChannel = application.getAdminContext().getBean("deployChannel",
+				AbstractMessageChannel.class);
+		AbstractMessageChannel undeployChannel = application.getAdminContext().getBean("undeployChannel",
+				AbstractMessageChannel.class);
+		deployChannel.addInterceptor(new WireTap(tapChannel));
+		undeployChannel.addInterceptor(new WireTap(tapChannel));
+		context.addApplicationListener(this.moduleEventListener);
+		setupApplicationContext(context);
+	}
+
+	@After
+	public final void shutDown() {
+		cleanup(this.context);
+		this.application.close();
+	}
+
+	@Test
+	public final void deployAndUndeploy() throws InterruptedException {
+
+		assertEquals(0, streamRepository.count());
+		final int ITERATIONS = 5;
+		int i = 0;
+		for (i = 0; i < ITERATIONS; i++) {
+			StreamDefinition definition = new StreamDefinition("test" + i,
+					"http | transform --expression=payload | filter --expression=true | log");
+			streamDeployer.save(definition);
+			waitForDeploy(definition);
+			assertEquals(1, streamRepository.count());
+			assertTrue(streamRepository.exists("test" + i));
+			waitForUndeploy(definition);
+			assertEquals(0, streamRepository.count());
+			assertFalse(streamRepository.exists("test" + i));
+			// Deploys in reverse order
+			assertModuleRequest("log", false);
+			assertModuleRequest("filter", false);
+			assertModuleRequest("transform", false);
+			assertModuleRequest("http", false);
+			// Undeploys in stream order
+			assertModuleRequest("http", true);
+			assertModuleRequest("transform", true);
+			assertModuleRequest("filter", true);
+			assertModuleRequest("log", true);
+			assertNull(tapChannel.receive(0));
+		}
+		assertEquals(ITERATIONS, i);
+
+	}
+
+	protected void assertModuleRequest(String moduleName, boolean remove) {
+		Message<?> next = tapChannel.receive(0);
+		assertNotNull(next);
+		String payload = (String) next.getPayload();
+
+		assertTrue(payload.contains("\"module\":\"" + moduleName + "\""));
+		assertTrue(payload.contains("\"remove\":" + (remove ? "true" : "false")));
+	}
+
+	protected void setupApplicationContext(ApplicationContext context) {
+	}
+
+	protected abstract String getTransport();
+
+	protected abstract void cleanup(ApplicationContext context);
+
+	protected Module getModule(String moduleName, int index) {
+
+		final Map<String, Map<Integer, Module>> deployedModules = this.moduleEventListener.getDeployedModules();
+
+		Module matchedModule = null;
+		for (Entry<String, Map<Integer, Module>> entry : deployedModules.entrySet()) {
+			final Module module = entry.getValue().get(index);
+			if (module != null && moduleName.equals(module.getName())) {
+				matchedModule = module;
+				break;
+			}
+		}
+		return matchedModule;
+	}
+
+	protected void deploy(StreamDefinition definition) {
+		waitForDeploy(definition);
+	}
+
 	private void doTest(StreamDefinition routerDefinition) throws InterruptedException {
 		assertEquals(0, streamRepository.count());
 		streamDeployer.save(routerDefinition);
@@ -134,5 +255,68 @@ public abstract class AbstractSingleNodeStreamDeploymentIntegrationTests extends
 		bus.unbindConsumer("queue:bar", barChannel);
 	}
 
+	private boolean waitForStreamOp(StreamDefinition definition, boolean isDeploy) {
+		final int MAX_TRIES = 40;
+		int tries = 1;
+		boolean done = false;
+		while (!done && tries <= MAX_TRIES) {
+			done = true;
+			int i = definition.getModuleDefinitions().size();
+			for (ModuleDefinition module : definition.getModuleDefinitions()) {
+				Module deployedModule = getModule(module.getName(), --i);
 
+				done = (isDeploy) ? deployedModule != null : deployedModule == null;
+				if (!done) {
+					break;
+				}
+			}
+			if (!done) {
+				try {
+					Thread.sleep(100);
+					tries++;
+				}
+				catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+				}
+			}
+		}
+		return done;
+	}
+
+	private void waitForUndeploy(StreamDefinition definition) {
+		streamDeployer.undeploy(definition.getName());
+		boolean undeployed = waitForStreamOp(definition, false);
+		assertTrue("stream " + definition.getName() + " not undeployed ", undeployed);
+	}
+
+	private void waitForDeploy(StreamDefinition definition) {
+
+		streamDeployer.deploy(definition.getName());
+		boolean deployed = waitForStreamOp(definition, true);
+		assertTrue("stream " + definition.getName() + " not deployed ", deployed);
+	}
+
+	static class ModuleEventListener implements ApplicationListener<AbstractModuleEvent> {
+
+		private final ConcurrentMap<String, Map<Integer, Module>> deployedModules = new ConcurrentHashMap<String, Map<Integer, Module>>();
+
+		@Override
+		public void onApplicationEvent(AbstractModuleEvent event) {
+			Module module = event.getSource();
+			if (event.getType().equals("ModuleDeployed")) {
+				this.deployedModules.putIfAbsent(module.getDeploymentMetadata().getGroup(),
+						new HashMap<Integer, Module>());
+				this.deployedModules.get(module.getDeploymentMetadata().getGroup()).put(
+						module.getDeploymentMetadata().getIndex(), module);
+			}
+			else {
+				this.deployedModules.get(module.getDeploymentMetadata().getGroup()).remove(
+						module.getDeploymentMetadata().getIndex());
+			}
+		}
+
+		public Map<String, Map<Integer, Module>> getDeployedModules() {
+			return this.deployedModules;
+		}
+	}
 }

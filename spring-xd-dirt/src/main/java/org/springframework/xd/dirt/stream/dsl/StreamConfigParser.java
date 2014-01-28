938a17c648b3d11adf6f99cf9e7f95e0874e84c0
David Turanski <dturanski_at_gopivotal_dot_com>
2014-01-28 16:54:58 -0500

    XD-1173 restored StreamConfigParser

--- spring-xd-dirt/src/main/java/org/springframework/xd/dirt/stream/dsl/StreamConfigParser.java
+++ spring-xd-dirt/src/main/java/org/springframework/xd/dirt/stream/dsl/StreamConfigParser.java
@@ -25,7 +25,6 @@ import org.springframework.xd.dirt.core.BaseDefinition;
 
 /**
  * @author Andy Clement
- * @author David Turanski
  */
 public class StreamConfigParser implements StreamLookupEnvironment {
 

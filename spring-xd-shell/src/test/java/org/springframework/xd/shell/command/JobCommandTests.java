6cf2f2f0a9d04ff3a04bf4c945fe03d4e6680ad0
Gary Russell <grussell@gopivotal.com>
2014-01-31 09:02:35 -0500

    Polishing - PR Comment
    Fix loop waiting for job to stop.

--- spring-xd-shell/src/test/java/org/springframework/xd/shell/command/JobCommandTests.java
+++ spring-xd-shell/src/test/java/org/springframework/xd/shell/command/JobCommandTests.java
@@ -432,21 +432,21 @@ public class JobCommandTests extends AbstractJobIntegrationTest {
 		executeCommand("job execution stop " + executionId);
 		// sleep for stop() until the step2 is invoked.
 		Thread.sleep(3000);
-		table = (Table) executeCommand("job execution list").getResult();
 		int n = 0;
-		while (!"STOPPED".equals(executionStatus) && n++ < 100) {
+		do {
+			table = (Table) executeCommand("job execution list").getResult();
 			for (TableRow tr : table.getRows()) {
 				// Match by above executionId
 				if (tr.getValue(1).equals(executionId)) {
 					executionStatus = tr.getValue(5);
-					if (!"STOPPED".equals(executionStatus)) {
-						Thread.sleep(100);
-						table = (Table) executeCommand("job execution list").getResult();
-					}
 					break;
 				}
 			}
+			if (!"STOPPED".equals(executionStatus)) {
+				Thread.sleep(100);
+			}
 		}
+		while (!"STOPPED".equals(executionStatus) && n++ < 100);
 		assertEquals("STOPPED", executionStatus);
 	}
 

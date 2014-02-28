79d723ec227ede7194912d118d5c7d298f18ce5f
Mark Fisher <mfisher@gopivotal.com>
2014-02-27 21:14:43 -0500

    XD-1358
    Removed unnecessary config files.
    
    Renamed container.xml to container-server.xml (which is imported by ContainerServerApplication) to be consistent with admin-server.xml (which is imported by AdminServerApplication).

--- spring-xd-dirt/src/main/java/org/springframework/xd/dirt/server/ContainerServerApplication.java
+++ spring-xd-dirt/src/main/java/org/springframework/xd/dirt/server/ContainerServerApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 the original author or authors.
+ * Copyright 2013-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,8 +42,7 @@ import org.springframework.xd.dirt.util.XdConfigLoggingInitializer;
 @Configuration
 @EnableAutoConfiguration
 @ImportResource({
-	"classpath:" + ConfigLocations.XD_INTERNAL_CONFIG_ROOT + "launcher.xml",
-	"classpath:" + ConfigLocations.XD_INTERNAL_CONFIG_ROOT + "container.xml",
+	"classpath:" + ConfigLocations.XD_INTERNAL_CONFIG_ROOT + "container-server.xml",
 	"classpath*:" + ConfigLocations.XD_CONFIG_ROOT + "plugins/*.xml" })
 public class ContainerServerApplication {
 

0c066a5cbeea7d4b60b29ad974fdd85a6f152b80
Luke Taylor <luke.taylor@springsource.com>
2014-02-28 13:33:54 +0000

    XD-1105: Simple mqtt integration test
    Add MqttTestSupport rule.
    
    Note that none of the RabbitSingleNodeStreamDeploymentIntegrationTests
    will run if matt is not available as this stands. However it is trivial
    to enable the matt plugin in rabbit so this is cleaner than splitting
    out a separate rabbit integration tests class for the matt test.

--- spring-xd-dirt/src/test/java/org/springframework/xd/dirt/stream/RabbitSingleNodeStreamDeploymentIntegrationTests.java
+++ spring-xd-dirt/src/test/java/org/springframework/xd/dirt/stream/RabbitSingleNodeStreamDeploymentIntegrationTests.java
@@ -13,13 +13,21 @@
 
 package org.springframework.xd.dirt.stream;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Test;
 import org.junit.rules.ExternalResource;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.context.ApplicationContext;
 import org.springframework.integration.x.bus.RabbitTestMessageBus;
+import org.springframework.xd.dirt.integration.test.sink.NamedChannelSink;
+import org.springframework.xd.dirt.integration.test.sink.SingleNodeNamedChannelSinkFactory;
+import org.springframework.xd.dirt.integration.test.source.NamedChannelSource;
+import org.springframework.xd.dirt.integration.test.source.SingleNodeNamedChannelSourceFactory;
+import org.springframework.xd.test.mqtt.MqttTestSupport;
 import org.springframework.xd.test.rabbit.RabbitTestSupport;
 
 /**
@@ -31,6 +39,9 @@ public class RabbitSingleNodeStreamDeploymentIntegrationTests extends
 
 	@ClassRule
 	public static RabbitTestSupport rabbitAvailableRule = new RabbitTestSupport();
+
+	@ClassRule
+	public static MqttTestSupport mqttAvailableRule = new MqttTestSupport();
 
 	@BeforeClass
 	public static void setUp() {
@@ -56,5 +67,24 @@ public class RabbitSingleNodeStreamDeploymentIntegrationTests extends
 		String undeployerExchange = context.getEnvironment().resolvePlaceholders(XD_UNDEPLOYER_PLACEHOLDER);
 		admin.deleteQueue(deployerQueue);
 		admin.deleteExchange(undeployerExchange);
+	}
+
+	@Test
+	public void mqttSourceStreamReceivesMqttSinkStreamOutput() throws Exception {
+		StreamDefinition mqtt1 = new StreamDefinition("mqtt1", "queue:mqttsource > mqtt --topic=foo");
+		StreamDefinition mqtt2 = new StreamDefinition("mqtt2", "mqtt --topics=foo > queue:mqttsink");
+		integrationSupport.createAndDeployStream(mqtt1);
+		integrationSupport.createAndDeployStream(mqtt2);
+
+		NamedChannelSource source = new SingleNodeNamedChannelSourceFactory(integrationSupport.messageBus()).createNamedChannelSource("queue:mqttsource");
+		NamedChannelSink sink = new SingleNodeNamedChannelSinkFactory(integrationSupport.messageBus()).createNamedChannelSink("queue:mqttsink");
+
+		Thread.sleep(1000);
+		source.sendPayload("hello");
+		Object result = sink.receivePayload(1000);
+
+		assertEquals("hello", result);
+		source.unbind();
+		sink.unbind();
 	}
 }

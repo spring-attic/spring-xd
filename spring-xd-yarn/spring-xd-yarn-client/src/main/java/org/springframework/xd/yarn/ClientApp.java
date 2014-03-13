4fd211110df7d2fd09e4e57a5c0fd6d110113fdb
Janne Valkealahti <janne.valkealahti@gmail.com>
2014-03-13 16:30:55 +0000

    XD-1386 Separating YARN app install from submit
    - Merge and squashed of Thomas PR 632
    - Adding new xd-install.sh shell script for copying app artifacts to HDFS
    - Client app now takes parameter of --install and --submit with --submit the default

--- spring-xd-yarn/spring-xd-yarn-client/src/main/java/org/springframework/xd/yarn/ClientApp.java
+++ spring-xd-yarn/spring-xd-yarn-client/src/main/java/org/springframework/xd/yarn/ClientApp.java
@@ -2,14 +2,41 @@ package org.springframework.xd.yarn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.yarn.client.YarnClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableAutoConfiguration
 public class ClientApp {
 	public static void main(String[] args) {
-		SpringApplication.run(ClientApp.class, args)
-				.getBean(YarnClient.class)
-				.submitApplication();
+		boolean install = false;
+		boolean submit = false;
+		List<String> appArgs = new ArrayList<String>();
+		for (String arg : args) {
+			if ("--install".equals(arg)) {
+				install = true;
+			} else {
+				if ("--submit".equals(arg)) {
+					submit = true;
+				} else {
+					appArgs.add(arg);
+				}
+			}
+		}
+		if (!submit && !install) {
+			submit = true;
+		}
+		ConfigurableApplicationContext ctx =
+				SpringApplication.run(ClientApp.class, appArgs.toArray(new String[appArgs.size()]));
+		YarnClient client = ctx.getBean(YarnClient.class);
+		if (install) {
+			client.installApplication();
+		}
+		if (submit) {
+			client.submitApplication(false);
+		}
 	}
 
 }

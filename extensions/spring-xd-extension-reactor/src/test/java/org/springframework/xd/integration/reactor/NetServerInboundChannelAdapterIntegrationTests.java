36af2473924901f9c243cef0d9a83561de1b6509
Jon Brisbin <jon@jbrisbin.com>
2014-03-18 11:26:26 -0500

    XD-1401 Added reactor-tcp source module definition. Updated NetServer configuration to use individual properties that can be referenced from the CLI using OptionsMetadata.
    Also addresses XD-1407 by adding a simple ThroughputSamplerMessageHandler that counts messages and reports throughput by recognizing demarcation/control messages. The message payloads are configurable but the default is to count messages between START and END messages.

--- extensions/spring-xd-extension-reactor/src/test/java/org/springframework/xd/integration/reactor/NetServerInboundChannelAdapterIntegrationTests.java
+++ extensions/spring-xd-extension-reactor/src/test/java/org/springframework/xd/integration/reactor/NetServerInboundChannelAdapterIntegrationTests.java
@@ -16,7 +16,26 @@
 
 package org.springframework.xd.integration.reactor;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+import org.springframework.integration.channel.PublishSubscribeChannel;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.validation.BindException;
+import org.springframework.xd.integration.reactor.net.NetServerInboundChannelAdapterConfiguration;
+import org.springframework.xd.integration.reactor.net.NetServerSourceOptionsMetadata;
+import org.springframework.xd.module.options.PojoModuleOptionsMetadata;
+import reactor.net.tcp.support.SocketUtils;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -29,27 +48,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.env.PropertySource;
-import org.springframework.integration.channel.PublishSubscribeChannel;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.MessagingException;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.util.StringValueResolver;
-import org.springframework.validation.BindException;
-import org.springframework.xd.integration.reactor.net.NetServerInboundChannelAdapterConfiguration;
-import org.springframework.xd.integration.reactor.net.NetServerSourceOptionsMetadata;
-import org.springframework.xd.module.options.PojoModuleOptionsMetadata;
-
-import reactor.net.tcp.support.SocketUtils;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Jon Brisbin
@@ -58,104 +57,105 @@ import reactor.net.tcp.support.SocketUtils;
 @ContextConfiguration(classes = NetServerInboundChannelAdapterIntegrationTests.BaseConfig.class)
 public class NetServerInboundChannelAdapterIntegrationTests {
 
-	@Autowired
-	Executor executor;
+  @Autowired
+  Executor executor;
 
-	@Autowired
-	PublishSubscribeChannel output;
+  @Autowired
+  PublishSubscribeChannel output;
 
-	@Autowired
-	StringWriter writer;
+  @Autowired
+  StringWriter writer;
 
-	@Test
-	public void netServerIngestsMessages() throws InterruptedException {
-		final CountDownLatch latch = new CountDownLatch(2);
-		output.subscribe(new MessageHandler() {
+  @Test
+  public void netServerIngestsMessages() throws InterruptedException {
+    final CountDownLatch latch = new CountDownLatch(2);
+    output.subscribe(new MessageHandler() {
 
-			@Override
-			public void handleMessage(Message<?> message) throws MessagingException {
-				if ("Hello World!".equals(message.getPayload())) {
-					latch.countDown();
-				}
-			}
-		});
+      @Override
+      public void handleMessage(Message<?> message) throws MessagingException {
+        if ("Hello World!".equals(message.getPayload())) {
+          latch.countDown();
+        }
+      }
+    });
 
-		executor.execute(writer);
+    executor.execute(writer);
 
-		assertTrue("latch was counted down", latch.await(5, TimeUnit.SECONDS));
-	}
+    assertTrue("latch was counted down", latch.await(5, TimeUnit.SECONDS));
+  }
 
-	@Configuration
-	static class TestConfig {
+  @Configuration
+  static class TestConfig {
 
-		int port = SocketUtils.findAvailableTcpPort();
+    int port = SocketUtils.findAvailableTcpPort();
 
-		@Bean
-		public StringValueResolver optionsMetadataValueResolver() {
-			NetServerSourceOptionsMetadata meta = new NetServerSourceOptionsMetadata();
-			meta.setBind("tcp://0.0.0.0:" + port + "/linefeed?codec=string");
-			return new OptionsMetadataValueResolver(meta);
-		}
+    @Bean
+    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() throws BindException {
+      PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+      MutablePropertySources ps = new MutablePropertySources();
+      ps.addFirst(optionsMetadataPropertySource());
+      pspc.setPropertySources(ps);
+      return pspc;
+    }
 
-		@Bean
-		public PropertySource optionsMetadataPropertySource() throws BindException {
-			Map<String, String> opts = new HashMap<>();
-			opts.put("bind", "tcp://0.0.0.0:" + port + "/linefeed?codec=string");
+    @Bean
+    public PropertySource optionsMetadataPropertySource() throws BindException {
+      Map<String, String> opts = new HashMap<>();
+      opts.put("port", String.valueOf(port));
 
-			return new PojoModuleOptionsMetadata(NetServerSourceOptionsMetadata.class)
-					.interpolate(opts)
-					.asPropertySource();
-		}
+      return new PojoModuleOptionsMetadata(NetServerSourceOptionsMetadata.class)
+          .interpolate(opts)
+          .asPropertySource();
+    }
 
-		@Bean
-		public StringWriter writer() {
-			return new StringWriter(port);
-		}
+    @Bean
+    public StringWriter writer() {
+      return new StringWriter(port);
+    }
 
-		@Bean
-		public Executor executor() {
-			return Executors.newCachedThreadPool();
-		}
+    @Bean
+    public Executor executor() {
+      return Executors.newCachedThreadPool();
+    }
 
-		@Bean
-		public PublishSubscribeChannel output() {
-			return new PublishSubscribeChannel();
-		}
+    @Bean
+    public PublishSubscribeChannel output() {
+      return new PublishSubscribeChannel();
+    }
 
-	}
+  }
 
-	@Configuration
-	@Import({
-		TestConfig.class,
-		NetServerInboundChannelAdapterConfiguration.class
-	})
-	static class BaseConfig {
-	}
+  @Configuration
+  @Import({
+      TestConfig.class,
+      NetServerInboundChannelAdapterConfiguration.class
+  })
+  static class BaseConfig {
+  }
 
-	static class StringWriter implements Runnable {
+  static class StringWriter implements Runnable {
 
-		final String line = "Hello World!\n";
+    final String line = "Hello World!\n";
 
-		final int port;
+    final int port;
 
-		StringWriter(int port) {
-			this.port = port;
-		}
+    StringWriter(int port) {
+      this.port = port;
+    }
 
-		@Override
-		public void run() {
-			try {
-				SocketChannel ch = SocketChannel.open();
-				ch.configureBlocking(true);
-				ch.connect(new InetSocketAddress(port));
-				ch.write(ByteBuffer.wrap(line.getBytes()));
-				ch.write(ByteBuffer.wrap(line.getBytes()));
-				ch.close();
-			}
-			catch (IOException e) {
-				throw new IllegalStateException(e);
-			}
-		}
-	}
+    @Override
+    public void run() {
+      try {
+        SocketChannel ch = SocketChannel.open();
+        ch.configureBlocking(true);
+        ch.connect(new InetSocketAddress(port));
+        ch.write(ByteBuffer.wrap(line.getBytes()));
+        ch.write(ByteBuffer.wrap(line.getBytes()));
+        ch.close();
+      } catch (IOException e) {
+        throw new IllegalStateException(e);
+      }
+    }
+  }
 
 }

ec2ad8f007cf67c353334c5a79d68ba839102586
Mark Fisher <mfisher@gopivotal.com>
2014-04-03 21:55:57 -0400

    fix race condition for stream properties

--- spring-xd-shell/src/test/java/org/springframework/xd/shell/StreamCommandListener.java
+++ spring-xd-shell/src/test/java/org/springframework/xd/shell/StreamCommandListener.java
@@ -17,11 +17,19 @@
 package org.springframework.xd.shell;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
@@ -44,7 +52,7 @@ import org.springframework.xd.module.options.ModuleOptionsMetadataResolver;
 /**
  * A {@link PathChildrenCacheListener} that enables waiting for a stream to be created, deployed, undeployed or
  * destroyed.
- * 
+ *
  * @author David Turanski
  * @author Mark Fisher
  */
@@ -54,7 +62,8 @@ public class StreamCommandListener implements PathChildrenCacheListener {
 
 	private static int TIMEOUT = 5000;
 
-	private Map<String, Map<String, String>> streamProperties = new HashMap<String, Map<String, String>>();
+	private ConcurrentMap<String, SettableFuture<Map<String, String>>> streamProperties =
+			new ConcurrentHashMap<String, SettableFuture<Map<String, String>>>();
 
 	private volatile CuratorFramework client;
 
@@ -75,10 +84,25 @@ public class StreamCommandListener implements PathChildrenCacheListener {
 		StreamsPath path = new StreamsPath(event.getData().getPath());
 		log.info("event: {} stream: {}", path.getStreamName(), event.getType());
 		if (event.getType().equals(Type.CHILD_ADDED)) {
-			streamProperties.put(path.getStreamName(), mapBytesUtility.toMap(event.getData().getData()));
+			streamProperties.putIfAbsent(path.getStreamName(), new SettableFuture<Map<String, String>>());
+			streamProperties.get(path.getStreamName()).set(mapBytesUtility.toMap(event.getData().getData()));
 		}
 		else if (event.getType().equals(Type.CHILD_REMOVED)) {
 			streamProperties.remove(path.getStreamName());
+		}
+	}
+
+	private Map<String, String> getStreamProperties(String streamName, long timeout) {
+		this.streamProperties.putIfAbsent(streamName, new SettableFuture<Map<String, String>>());
+		try {
+			return this.streamProperties.get(streamName).get(timeout, TimeUnit.MILLISECONDS);
+		}
+		catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			return null;
+		}
+		catch (Exception e) {
+			throw new IllegalStateException("failed while waiting for stream properties", e);
 		}
 	}
 
@@ -126,7 +150,7 @@ public class StreamCommandListener implements PathChildrenCacheListener {
 				}
 				catch (Exception e) {
 					throw new IllegalStateException(String.format(
-							"Failed while waiting for deployment of stream %s.", streamName));
+							"Failed while waiting for deployment of stream %s.", streamName), e);
 				}
 			}
 		}
@@ -158,7 +182,7 @@ public class StreamCommandListener implements PathChildrenCacheListener {
 			}
 			catch (Exception e) {
 				throw new IllegalStateException(String.format(
-						"Failed while waiting for undeployment of stream %s.", streamName));
+						"Failed while waiting for undeployment of stream %s.", streamName), e);
 			}
 		}
 		while (System.currentTimeMillis() < timeout);
@@ -168,7 +192,7 @@ public class StreamCommandListener implements PathChildrenCacheListener {
 	private List<String> getModuleDeploymentPaths(String streamName) {
 		List<String> moduleDeploymentPaths = new ArrayList<String>();
 		try {
-			Stream stream = streamFactory.createStream(streamName, streamProperties.get(streamName));
+			Stream stream = streamFactory.createStream(streamName, getStreamProperties(streamName, 5000));
 			for (Iterator<ModuleDescriptor> iterator = stream.getDeploymentOrderIterator(); iterator.hasNext();) {
 				ModuleDescriptor descriptor = iterator.next();
 				moduleDeploymentPaths.add(new StreamsPath()
@@ -180,9 +204,59 @@ public class StreamCommandListener implements PathChildrenCacheListener {
 		}
 		catch (Exception e) {
 			throw new IllegalStateException(
-					String.format("Failed to determine module deployment paths for stream %s", streamName));
+					String.format("Failed to determine module deployment paths for stream %s", streamName), e);
 		}
 		return moduleDeploymentPaths;
+	}
+
+	private static class SettableFuture<V> implements Future<V> {
+
+		private final AtomicReference<V> result = new AtomicReference<V>();
+
+		private final CountDownLatch latch = new CountDownLatch(1);
+
+		private final AtomicBoolean cancelled = new AtomicBoolean();
+
+		@Override
+		public boolean cancel(boolean mayInterruptIfRunning) {
+			return cancelled.compareAndSet(false, true);
+		}
+
+		@Override
+		public boolean isCancelled() {
+			return cancelled.get();
+		}
+
+		@Override
+		public boolean isDone() {
+			return cancelled.get() || (result.get() != null);
+		}
+
+		public void set(V value) {
+			result.set(value);
+			latch.countDown();
+		}
+
+		@Override
+		public V get() throws InterruptedException, ExecutionException {
+			if (cancelled.get()) {
+				return null;
+			}
+			latch.await();
+			return result.get();
+		}
+
+		@Override
+		public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException,
+				TimeoutException {
+			if (cancelled.get()) {
+				return null;
+			}
+			if (!latch.await(timeout, unit)) {
+				throw new TimeoutException();
+			}
+			return result.get();
+		}
 	}
 
 }

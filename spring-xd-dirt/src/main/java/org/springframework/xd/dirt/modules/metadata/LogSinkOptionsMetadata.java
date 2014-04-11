1eedc6a8729918baaa787b6fd076610fa09abaf6
Eric Bottard <ebottard@gopivotal.com>
2014-04-11 13:46:46 +0200

    XD-1378: Add options to the log module

---
+++ spring-xd-dirt/src/main/java/org/springframework/xd/dirt/modules/metadata/LogSinkOptionsMetadata.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.xd.dirt.modules.metadata;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import org.springframework.xd.module.options.spi.ModuleOption;
+import org.springframework.xd.module.options.spi.ModulePlaceholders;
+
+
+/**
+ * Captures options for the {@code log} sink module.
+ * 
+ * @author Eric Bottard
+ */
+public class LogSinkOptionsMetadata {
+
+	private String name = ModulePlaceholders.XD_STREAM_NAME;
+
+	@NotBlank
+	public String getName() {
+		return name;
+	}
+
+	@ModuleOption("the name of the log category to log to")
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}

82b8a44bec168f667b7673c7ca91843859d61392
David Turanski <dturanski_at_gopivotal_dot_com>
2014-04-17 10:04:54 -0400

    XD-1490 support absolute path in CacheServer

--- spring-xd-gemfire-server/src/main/java/org/springframework/xd/gemfire/CacheServer.java
+++ spring-xd-gemfire-server/src/main/java/org/springframework/xd/gemfire/CacheServer.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2002-2013 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -28,7 +28,7 @@ import com.gemstone.gemfire.management.ManagementService;
 
 /**
  * @author David Turanski
- * 
+ *
  */
 public class CacheServer {
 
@@ -41,7 +41,14 @@ public class CacheServer {
 			System.out.println("Usage: CacheServer <config-file-path>");
 			System.exit(1);
 		}
+
+		/*
+		 * Ensure absolute path is handled
+		 */
 		String path = args[0];
+		if (!path.startsWith("file:")) {
+			path = "file:" + path;
+		}
 
 		if (!portAvailable(port)) {
 			System.out.println("Cache Server port " + port + " is not available. Is another instance running?");

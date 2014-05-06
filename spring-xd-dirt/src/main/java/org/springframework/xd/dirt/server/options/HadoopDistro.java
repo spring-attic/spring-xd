b5c3c0e1f770ea8e5e80af24f43bf5981c10aa85
Thomas Risberg <trisberg@gopivotal.com>
2014-05-06 14:15:49 -0400

    XD-1446 Update spring-data-hadoop dependency and add new Hadoop distros
    - Changing Spring for Apache Hadoop dependency to 2.0.0.RC3
    
    - Adding Pivotal HD 2.0 as distro (phd20)
    
    - Adding Cloudera CDH5 as distro (cdh5)
    
    - Changing Hortonworks Data Platform version 2.0 to 2.1 (hdp21)

--- spring-xd-dirt/src/main/java/org/springframework/xd/dirt/server/options/HadoopDistro.java
+++ spring-xd-dirt/src/main/java/org/springframework/xd/dirt/server/options/HadoopDistro.java
@@ -20,5 +20,5 @@ package org.springframework.xd.dirt.server.options;
  * @author Thomas Risberg
  */
 public enum HadoopDistro {
-	hadoop12, hadoop22, phd1, cdh4, hdp13, hdp20
+	hadoop12, hadoop22, phd1, phd20, cdh4, cdh5, hdp13, hdp21
 }

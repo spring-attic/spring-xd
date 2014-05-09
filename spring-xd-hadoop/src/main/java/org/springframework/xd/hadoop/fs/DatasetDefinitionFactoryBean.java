b513890197a3d44e7adc4c848b8934a6e12a250e
Thomas Risberg <trisberg@gopivotal.com>
2014-05-09 16:00:37 -0400

    XD-997 Adding initial partitioning support for hdfs-dataset sink

---
+++ spring-xd-hadoop/src/main/java/org/springframework/xd/hadoop/fs/DatasetDefinitionFactoryBean.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.xd.hadoop.fs;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.kitesdk.data.PartitionStrategy;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.data.hadoop.store.StoreException;
+import org.springframework.data.hadoop.store.dataset.DatasetDefinition;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.SpelEvaluationException;
+import org.springframework.expression.spel.SpelParseException;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * FactoryBean that creates a default DatasetDefinition
+ *
+ * @author Thomas Risberg
+ */
+public class DatasetDefinitionFactoryBean implements InitializingBean, FactoryBean<DatasetDefinition>{
+
+	private final Log logger = LogFactory.getLog(this.getClass());
+
+	String format;
+
+	Boolean allowNullValues;
+
+	String partitionPath;
+
+	DatasetDefinition datasetDefinition;
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+	public void setAllowNullValues(Boolean allowNullValues) {
+		this.allowNullValues = allowNullValues;
+	}
+
+	public void setPartitionPath(String partitionPath) {
+		this.partitionPath = partitionPath;
+	}
+
+	@Override
+	public DatasetDefinition getObject() throws Exception {
+		return datasetDefinition;
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return DatasetDefinition.class;
+	}
+
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		logger.info("DatasetDefinition properties: ");
+		logger.info("  allowNulValues: " + allowNullValues);
+		logger.info("          format: " + format);
+		logger.info("   partitionPath: " + partitionPath);
+		datasetDefinition = new DatasetDefinition(allowNullValues, format);
+		if (StringUtils.hasText(partitionPath)) {
+			datasetDefinition.setPartitionStrategy(parsePartitionExpression(partitionPath));
+		}
+	}
+
+	private static PartitionStrategy parsePartitionExpression(String expression) {
+
+		List<String> expressions = Arrays.asList(expression.split("/"));
+
+		ExpressionParser parser = new SpelExpressionParser();
+		PartitionStrategy.Builder psb = new PartitionStrategy.Builder();
+		StandardEvaluationContext ctx = new StandardEvaluationContext(psb);
+		for (String expr : expressions) {
+			try {
+				Expression e = parser.parseExpression(expr);
+				psb = e.getValue(ctx, PartitionStrategy.Builder.class);
+			} catch (SpelParseException spe) {
+				if (!expr.trim().endsWith(")")) {
+					throw new StoreException("Invalid partitioning expression '" + expr + "' -  did you forget the closing parenthesis?", spe);
+				} else {
+					throw new StoreException("Invalid partitioning expression '" + expr + "'!", spe);
+				}
+			} catch (SpelEvaluationException see) {
+				throw new StoreException("Invalid partitioning expression '" + expr + "' - failed evaluation!", see);
+			} catch (NullPointerException npe) {
+				throw new StoreException("Invalid partitioning expression '" + expr + "' - was evaluated to null!", npe);
+			}
+		}
+		return psb.build();
+	}
+
+}

d94513be4cfffcb1bfa748816f76c96203faab30
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2014-08-20 12:08:06 -0700

    XD-1329 Add Kafka source module
    - Add kafka source module with consumer configurations
    - Add `spring-xd-extension-kafka` subproject
      - Add Kafka module options mixin and  source module options metadata
    
    Fix code review comments
      - Add kakfa zkconnect chroot path for kafka zk namespace
         - Allow the `zkconnect` property be overridden via modules.yml/kafka.properties
      - Update javadoc for StringDecoder
      - Update kafka inbound channel adapter's poller interval to `100ms` and
        max-messages per poll to `-1` which is unlimited.
    
    Add `encoding` module option for string ValueDecoder
    
    - Use `util:properties` for consumer-properties
      - Add config/kafka-consumer.properties to externalize the properites
    
    Add Kafka Message transformer
      - Message transformer that transforms the kafka messages into
    Map<Integer,Object> where key is partition index and value is payload.

---
+++ extensions/spring-xd-extension-kafka/src/main/java/org/springframework/integration/x/kafka/KafkaSourceModuleOptionsMetadata.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.integration.x.kafka;
+
+import org.springframework.xd.module.options.spi.Mixin;
+import org.springframework.xd.module.options.spi.ModuleOption;
+import org.springframework.xd.module.options.spi.ModulePlaceholders;
+
+
+/**
+ * Module options for Kafka source module.
+ *
+ * @author Ilayaperumal Gopinathan
+ */
+@Mixin(KafkaOptionMixin.class)
+public class KafkaSourceModuleOptionsMetadata {
+
+	private String topic = ModulePlaceholders.XD_STREAM_NAME;
+
+	private int streams = 1;
+
+	private String groupId = ModulePlaceholders.XD_STREAM_NAME;
+
+	private String encoding = "UTF8";
+
+	@ModuleOption("kafka topic name")
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public String getTopic() {
+		return this.topic;
+	}
+
+	@ModuleOption("number of streams in the topic")
+	public void setStreams(int streams) {
+		this.streams = streams;
+	}
+
+	public int getStreams() {
+		return this.streams;
+	}
+
+	@ModuleOption("kafka consumer configuration group id")
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+
+	public String getGroupId() {
+		return this.groupId;
+	}
+
+	@ModuleOption("string encoder to translate bytes into string")
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	public String getEncoding() {
+		return this.encoding;
+	}
+}

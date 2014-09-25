f7cf349308f1f8fe65cb91072821576f7322194c
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2014-09-25 09:20:46 -0700

    Add queue/store outbound adapter options
    Add integration tests

--- spring-xd-dirt/src/main/java/org/springframework/xd/dirt/modules/metadata/RedisSinkOptionsMetadata.java
+++ spring-xd-dirt/src/main/java/org/springframework/xd/dirt/modules/metadata/RedisSinkOptionsMetadata.java
@@ -16,9 +16,11 @@
 
 package org.springframework.xd.dirt.modules.metadata;
 
+import javax.validation.constraints.AssertTrue;
+
 import org.springframework.xd.module.options.spi.Mixin;
 import org.springframework.xd.module.options.spi.ModuleOption;
-import org.springframework.xd.module.options.spi.ModulePlaceholders;
+import org.springframework.xd.module.options.spi.ProfileNamesProvider;
 
 
 /**
@@ -27,16 +29,86 @@ import org.springframework.xd.module.options.spi.ModulePlaceholders;
  * @author Ilayaperumal Gopinathan
  */
 @Mixin(RedisConnectionMixin.class)
-public class RedisSinkOptionsMetadata {
+public class RedisSinkOptionsMetadata implements ProfileNamesProvider {
 
-	private String topic = "'" + ModulePlaceholders.XD_STREAM_NAME + "'";
+	private static final String TOPIC_PROFILE = "use-topic";
+
+	private static final String QUEUE_PROFILE = "use-queue";
+
+	private static final String KEY_PROFILE = "use-store";
+
+	private String topic = null;
+
+	private String queue = null;
+
+	private String key = null;
+
+	private String collectionType = "LIST";
 
 	public String getTopic() {
-		return topic;
+		return this.topic;
 	}
 
 	@ModuleOption("spel expression to use for topic")
 	public void setTopic(String topic) {
 		this.topic = topic;
+	}
+
+	public String getQueue() {
+		return this.queue;
+	}
+
+	@ModuleOption("spel expression to use for queue")
+	public void setQueue(String queue) {
+		this.queue = queue;
+	}
+
+
+	public String getKey() {
+		return this.key;
+	}
+
+	@ModuleOption("name of the redis store key to use")
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getCollectionType() {
+		return collectionType;
+	}
+
+
+	@ModuleOption("the collection type to use for the given key")
+	public void setCollectionType(String collectionType) {
+		this.collectionType = collectionType;
+	}
+
+	/**
+	 * User can't explicitly set mutually exclusive values together.
+	 */
+	@AssertTrue(message = "the 'topic', 'queue' and 'key' options are mutually exclusive")
+	public boolean isMutuallyExclusive() {
+		return (topic == null || queue != null || key == null) ||
+				(key == null || topic != null || queue == null) ||
+				(queue == null || key != null || topic == null);
+	}
+
+	@AssertTrue(message = "one of the 'topic', 'queue' or 'key' values must be set explicitly")
+	public boolean isValid() {
+		return ((queue != null) || (topic != null) || (key != null));
+	}
+
+	@Override
+	public String[] profilesToActivate() {
+		if (topic != null) {
+			return new String[] { TOPIC_PROFILE };
+		}
+		else if (queue != null) {
+			return new String[] { QUEUE_PROFILE };
+		}
+		else if (key != null) {
+			return new String[] { KEY_PROFILE };
+		}
+		return new String[] {};
 	}
 }

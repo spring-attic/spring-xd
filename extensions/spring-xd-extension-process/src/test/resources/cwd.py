d16141aa5cf601ebb19e19240f96541a2b943153
David Turanski <dturanski_at_gopivotal_dot_com>
2014-10-25 08:17:50 -0400

    Fixed ShellProcessorTests redux

--- extensions/spring-xd-extension-process/src/test/resources/cwd.py
+++ extensions/spring-xd-extension-process/src/test/resources/cwd.py
@@ -1,2 +1,6 @@
 import os
+import sys
+import time
 print os.getcwd()
+sys.stdout.flush()
+time.sleep(10)
--- extensions/spring-xd-extension-process/src/test/resources/env.py
+++ extensions/spring-xd-extension-process/src/test/resources/env.py
@@ -1,2 +1,6 @@
 import os
+import sys
+import time
 print os.environ['FOO'] + os.environ['BAR']
+sys.stdout.flush()
+time.sleep(10)

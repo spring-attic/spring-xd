a792ddd63154a66db104890697419c6fbdb9e11c
Thomas Risberg <trisberg@gopivotal.com>
2015-01-05 11:33:02 -0500

    Fix date format in failing test

--- spring-xd-hadoop/src/test/java/org/springframework/xd/integration/hadoop/config/DatasetOutboundChannelAdapterPartitionedIntegrationTests.java
+++ spring-xd-hadoop/src/test/java/org/springframework/xd/integration/hadoop/config/DatasetOutboundChannelAdapterPartitionedIntegrationTests.java
@@ -57,7 +57,7 @@ public class DatasetOutboundChannelAdapterPartitionedIntegrationTests {
 				new File(path + "/" + datasetOperations.getDatasetName(TestPojo.class)).exists());
 		assertTrue("Dataset metadata created",
 				new File(path + "/" + datasetOperations.getDatasetName(TestPojo.class) + "/.metadata").exists());
-		String year = new SimpleDateFormat("YYYY").format(t1.getTimestamp());
+		String year = new SimpleDateFormat("yyyy").format(t1.getTimestamp());
 		assertTrue("Dataset partition path created",
 				new File(path + "/" + datasetOperations.getDatasetName(TestPojo.class) + "/year=" + year).exists());
 		context.close();

f235a9663a9d892e207a2b4fb859fc132af7e555
Gary Russell <grussell@gopivotal.com>
2015-01-05 15:38:07 -0500

    Fix Licenses in Process Extension

--- extensions/spring-xd-extension-process/src/test/java/org/springframework/xd/extension/process/ShellCommandProcessorTests.java
+++ extensions/spring-xd-extension-process/src/test/java/org/springframework/xd/extension/process/ShellCommandProcessorTests.java
@@ -1,22 +1,23 @@
-package org.springframework.xd.extension.process;/*
+/*
+ * Copyright 2014-2015 the original author or authors.
  *
- *  * Copyright 2014 the original author or authors.
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  * http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-import static org.junit.Assert.*;
+package org.springframework.xd.extension.process;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.HashMap;
@@ -34,6 +35,7 @@ import org.springframework.integration.ip.tcp.serializer.ByteArrayLfSerializer;
  * @author David Turanski
  */
 public class ShellCommandProcessorTests {
+
 	private ShellCommandProcessor scp = null;
 
 	private AbstractByteArraySerializer serializer = new ByteArrayCrLfSerializer();
@@ -104,4 +106,5 @@ public class ShellCommandProcessorTests {
 		response = scp.sendAndReceive("echo");
 		assertEquals("echo", response);
 	}
+
 }

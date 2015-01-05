73c5237e7f0ef4f653113ed4cdbd14003cdd8cc0
Gary Russell <grussell@gopivotal.com>
2015-01-05 15:38:07 -0500

    Fix Licenses in Process Extension

--- extensions/spring-xd-extension-process/src/main/java/org/springframework/xd/extension/process/DelimitedStringToMapPropertyEditor.java
+++ extensions/spring-xd-extension-process/src/main/java/org/springframework/xd/extension/process/DelimitedStringToMapPropertyEditor.java
@@ -1,19 +1,17 @@
 /*
+ * Copyright 2014-2015 the original author or authors.
  *
- *  * Copyright 2014 the original author or authors.
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  * http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.springframework.xd.extension.process;
@@ -31,10 +29,11 @@ import org.springframework.util.StringUtils;
  * @author David Turanski
  */
 public class DelimitedStringToMapPropertyEditor extends PropertyEditorSupport {
+
 	@Override
 	@SuppressWarnings("unchecked")
 	public void setAsText(String text) {
-		Map<String,String> result = new HashMap<String,String>();
+		Map<String, String> result = new HashMap<String, String>();
 		if (!StringUtils.isEmpty(text)) {
 			String[] nameValuePairs = StringUtils.commaDelimitedListToStringArray(text);
 
@@ -43,7 +42,7 @@ public class DelimitedStringToMapPropertyEditor extends PropertyEditorSupport {
 				if (nameAndValue.length != 2) {
 					throw new RuntimeException("Invalid name-value pair: '" + text + "'");
 				}
-				result.put(nameAndValue[0],nameAndValue[1]);
+				result.put(nameAndValue[0], nameAndValue[1]);
 			}
 		}
 		setValue(result);

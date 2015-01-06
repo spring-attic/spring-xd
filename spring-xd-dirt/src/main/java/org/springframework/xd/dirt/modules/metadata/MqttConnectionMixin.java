3430f42d93d3a056017da9299ecd4ee003246746
Gary Russell <grussell@pivotal.io>
2015-01-06 12:03:35 -0500

    XD-2508: MQTT Enhancements
    JIRA: https://jira.spring.io/browse/XD-2508
    
    outbound: async, charset
    
    in and out: :multiple brokers, charset, cleanSession, connectionTimeout, keepAliveInterval, persistence

--- spring-xd-dirt/src/main/java/org/springframework/xd/dirt/modules/metadata/MqttConnectionMixin.java
+++ spring-xd-dirt/src/main/java/org/springframework/xd/dirt/modules/metadata/MqttConnectionMixin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2014 the original author or authors.
+ * Copyright 2013-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,14 +19,15 @@ package org.springframework.xd.dirt.modules.metadata;
 import org.hibernate.validator.constraints.NotBlank;
 
 import org.springframework.xd.module.options.spi.ModuleOption;
-
+import org.springframework.xd.module.options.spi.ProfileNamesProvider;
 
 /**
  * Factors out common options about configuring a connection to an MQTT broker.
  * 
  * @author Eric Bottard
+ * @author Gary Russell
  */
-public class MqttConnectionMixin {
+public class MqttConnectionMixin implements ProfileNamesProvider {
 
 	private String url = "tcp://localhost:1883";
 
@@ -34,12 +35,22 @@ public class MqttConnectionMixin {
 
 	private String password = "guest";
 
+	private boolean cleanSession = true;
+
+	private int connectionTimeout = 30;
+
+	private int keepAliveInterval = 60;
+
+	private String persistence = "memory";
+
+	private String persistenceDirectory = "/tmp/paho";
+
 	@NotBlank
 	public String getUrl() {
 		return url;
 	}
 
-	@ModuleOption("location of the mqtt broker")
+	@ModuleOption("location of the mqtt broker(s) (comma-delimited list)")
 	public void setUrl(String url) {
 		this.url = url;
 	}
@@ -61,4 +72,55 @@ public class MqttConnectionMixin {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public boolean isCleanSession() {
+		return cleanSession;
+	}
+
+	@ModuleOption("whether the client and server should remember state across restarts and reconnects")
+	public void setCleanSession(boolean cleanSession) {
+		this.cleanSession = cleanSession;
+	}
+
+	public int getKeepAliveInterval() {
+		return keepAliveInterval;
+	}
+
+	@ModuleOption("the ping interval in seconds")
+	public void setKeepAliveInterval(int keepAliveInterval) {
+		this.keepAliveInterval = keepAliveInterval;
+	}
+
+	public int getConnectionTimeout() {
+		return connectionTimeout;
+	}
+
+	@ModuleOption("the connection timeout in seconds")
+	public void setConnectionTimeout(int connectionTimeout) {
+		this.connectionTimeout = connectionTimeout;
+	}
+
+	public String getPersistence() {
+		return persistence;
+	}
+
+	@ModuleOption("'memory' or 'file'")
+	public void setPersistence(String persistence) {
+		this.persistence = persistence;
+	}
+
+	public String getPersistenceDirectory() {
+		return persistenceDirectory;
+	}
+
+	@ModuleOption("file location when using 'file' persistence")
+	public void setPersistenceDirectory(String persistenceDirectory) {
+		this.persistenceDirectory = persistenceDirectory;
+	}
+
+	@Override
+	public String[] profilesToActivate() {
+		return new String[] { this.persistence };
+	}
+
 }

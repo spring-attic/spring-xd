36bace5c1f7ee13475e4e898438382f1e9ab6576
Mark Pollack <mpollack@pivotal.io>
2015-01-09 16:17:08 -0500

    [Sprint 41] XD-2542 Create MessageHandler for RxJava based processor modules
    * Changed to use PublishSubject
    * Remove check for existing observers
    * Throw exception if can't dispatch message to a Subject
    * Keep track of Subscriptions, unsubscribe on app context destory
    * Remove Subject from map when it completes, will be created again on
      next message for a given dispatcher thread.

--- spring-xd-rxjava/src/main/java/org/springframework/xd/rxjava/SubjectMessageHandler.java
+++ spring-xd-rxjava/src/main/java/org/springframework/xd/rxjava/SubjectMessageHandler.java
@@ -17,36 +17,42 @@ package org.springframework.xd.rxjava;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.core.ResolvableType;
 import org.springframework.integration.handler.AbstractMessageProducingHandler;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandlingException;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.ReflectionUtils;
-
 import rx.Observable;
 import rx.Subscription;
+import rx.functions.Action0;
 import rx.functions.Action1;
-import rx.subjects.BehaviorSubject;
+import rx.subjects.PublishSubject;
 import rx.subjects.Subject;
 
 import java.lang.reflect.Method;
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
+ * A handler that adapts the item at a time delivery in a {@link org.springframework.messaging.MessageHandler}
+ * and delegates processing to an RxJava Observable.  A
  *
  * @author Mark Pollack
  */
-public class SubjectMessageHandler extends AbstractMessageProducingHandler {
+public class SubjectMessageHandler extends AbstractMessageProducingHandler implements DisposableBean {
 
     protected final Log logger = LogFactory.getLog(getClass());
 
-    private final ConcurrentMap<Long, BehaviorSubject<Object>> subjectMap =
-            new ConcurrentHashMap<Long, BehaviorSubject<Object>>();
+    private final ConcurrentMap<Long, PublishSubject<Object>> subjectMap =
+            new ConcurrentHashMap<Long, PublishSubject<Object>>();
 
-    private final Object monitor = new Object();
+    private final Map<Long, Subscription> subscriptionMap = new Hashtable<Long, Subscription>();
 
     @SuppressWarnings("rawtypes")
     private final Processor processor;
@@ -68,45 +74,56 @@ public class SubjectMessageHandler extends AbstractMessageProducingHandler {
         if (ClassUtils.isAssignable(inputType.getRawClass(), message.getClass())) {
             subjectToUse.onNext(message);
         } else if (ClassUtils.isAssignable(inputType.getRawClass(), message.getPayload().getClass())) {
-            //TODO handle type conversion of payload to input type if possible
-            System.out.println(Thread.currentThread().getName() +
-                    "> Inside handleMessageInternal, sending to subject data = " + message.getPayload());
             subjectToUse.onNext(message.getPayload());
+        } else {
+            throw new MessageHandlingException(message, "Processor signature does not match [" + message.getClass()
+                    + "] or [" + message.getPayload().getClass() + "]");
         }
-        //TODO else log error.
     }
 
     private Subject getSubject() {
         long idToUse = Thread.currentThread().getId();
         Subject subject = subjectMap.get(idToUse);
         if (subject == null) {
-            BehaviorSubject existingSubject = subjectMap.putIfAbsent(idToUse, BehaviorSubject.create());
+            PublishSubject existingSubject = subjectMap.putIfAbsent(idToUse, PublishSubject.create());
             if (existingSubject == null)
                 subject = subjectMap.get(idToUse);
-            synchronized (this.monitor) {
-                if (!subject.hasObservers()) {
-                    //user defined stream processing
-                    Observable<?> outputStream = processor.process(subject);
+            //user defined stream processing
+            Observable<?> outputStream = processor.process(subject);
 
-                    //TODO Error handling
-
-                    final Subscription subscription = outputStream.subscribe(new Action1<Object>() {
-                        @Override
-                        public void call(Object outputObject) {
-                            if (ClassUtils.isAssignable(Message.class, outputObject.getClass())) {
-                                getOutputChannel().send((Message) outputObject);
-                            } else {
-                                getOutputChannel().send(MessageBuilder.withPayload(outputObject).build());
-                            }
-                            //System.out.println(Thread.currentThread().getName() + "> Going to send to message bus " + o);
-                        }
-                    });
-
-                    //TODO keep track of subscriptions to unsubscribe cleanly...
+            final Subscription subscription = outputStream.subscribe(new Action1<Object>() {
+                @Override
+                public void call(Object outputObject) {
+                    if (ClassUtils.isAssignable(Message.class, outputObject.getClass())) {
+                        getOutputChannel().send((Message) outputObject);
+                    } else {
+                        getOutputChannel().send(MessageBuilder.withPayload(outputObject).build());
+                    }
                 }
-            }
+            }, new Action1<Throwable>() {
+                @Override
+                public void call(Throwable throwable) {
+                    logger.error(throwable);
+                }
+            }, new Action0() {
+                @Override
+                public void call() {
+                    subjectMap.remove(Thread.currentThread().getId());
+                }
+            });
+
+            subscriptionMap.put(idToUse, subscription);
+
+
         }
         return subject;
 
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        for (Subscription subscription : subscriptionMap.values()) {
+            subscription.unsubscribe();
+        }
     }
 }

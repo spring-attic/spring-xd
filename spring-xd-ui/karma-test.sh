528f11cb8085586cd250c25860ff5c5ea2ed3e51
Gunnar Hillert <ghillert@pivotal.io>
2015-03-24 10:41:03 -0400

    XD-2845 Add support for admin-ui and Flo integration
    * Separate AngularJS client application into the `spring-xd-ui-client` project
    * Remove Grunt/NPM related Gradle tasks

--- spring-xd-ui/karma-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BASE_DIR=.
-
-echo ""
-echo "Starting Karma Server (http://karma-runner.github.io)"
-echo "-------------------------------------------------------------------"
-
-karma start karma.conf.js $*
--- spring-xd-ui/test/protractor.conf.js
+++
@@ -1,6 +0,0 @@
-exports.config = {
-  specs: [
-    './e2e/**/*.spec.js'
-    ],
-  baseUrl: 'http://localhost:8000'
-}

d88a1f63c333d80d3603d457a3a4a53d3eeca088
Eric Bottard <ebottard@gopivotal.com>
2015-03-25 18:10:32 +0100

    Move module options gen

--- documentation-toolchain/src/main/java/org/springframework/xd/documentation/ModuleOptionsReferenceDoc.java
+++ documentation-toolchain/src/main/java/org/springframework/xd/documentation/ModuleOptionsReferenceDoc.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.xd.shell.util;
+package org.springframework.xd.documentation;
 
 import java.io.BufferedReader;
 import java.io.File;

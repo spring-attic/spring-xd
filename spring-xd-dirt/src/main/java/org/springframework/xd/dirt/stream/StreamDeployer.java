0d184685af1d5d3e26e8822e5ff66027336cef4c
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2015-03-25 11:07:24 -0700

    XD-2854 Migrate Stream definitions that don't have MDs
     - This code checks for existing stream definitions and update
    the module definitions if they don't exist.
     - Add better exception handling
     - Use PostConstruct to run migration

--- spring-xd-dirt/src/main/java/org/springframework/xd/dirt/stream/StreamDeployer.java
+++ spring-xd-dirt/src/main/java/org/springframework/xd/dirt/stream/StreamDeployer.java
@@ -18,9 +18,27 @@ package org.springframework.xd.dirt.stream;
 
 import static org.springframework.xd.dirt.stream.ParsingContext.stream;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.api.BackgroundPathAndBytesable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.xd.dirt.server.admin.deployment.DeploymentHandler;
 import org.springframework.xd.dirt.zookeeper.Paths;
 import org.springframework.xd.dirt.zookeeper.ZooKeeperConnection;
+import org.springframework.xd.dirt.zookeeper.ZooKeeperUtils;
+import org.springframework.xd.module.ModuleDefinition;
+import org.springframework.xd.module.ModuleDescriptor;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 /**
  * Default implementation of {@link StreamDeployer} that uses provided
@@ -33,8 +51,21 @@ import org.springframework.xd.dirt.zookeeper.ZooKeeperConnection;
  * @author Eric Bottard
  * @author Gunnar Hillert
  * @author Patrick Peralta
+ * @author Ilayaperumal Gopinathan
  */
 public class StreamDeployer extends AbstractInstancePersistingDeployer<StreamDefinition, Stream> {
+
+	private static final Logger logger = LoggerFactory.getLogger(StreamDeployer.class);
+
+	private final static TypeReference<List<ModuleDefinition>> MODULE_DEFINITIONS_LIST = new TypeReference<List<ModuleDefinition>>() {};
+
+	private final ObjectWriter objectWriter = new ObjectMapper().writerWithType(MODULE_DEFINITIONS_LIST);
+
+	private static final String DEFINITION_KEY = "definition";
+
+	private static final String MODULE_DEFINITIONS_KEY = "moduleDefinitions";
+
+	private final ZooKeeperConnection zkConnection;
 
 	/**
 	 * Stream definition parser.
@@ -52,6 +83,7 @@ public class StreamDeployer extends AbstractInstancePersistingDeployer<StreamDef
 	public StreamDeployer(ZooKeeperConnection zkConnection, StreamDefinitionRepository repository,
 			StreamRepository streamRepository, XDParser parser, DeploymentHandler deploymentHandler) {
 		super(zkConnection, repository, streamRepository, parser, deploymentHandler, stream);
+		this.zkConnection = zkConnection;
 		this.parser = parser;
 
 	}
@@ -78,6 +110,80 @@ public class StreamDeployer extends AbstractInstancePersistingDeployer<StreamDef
 	@Override
 	protected String getDeploymentPath(StreamDefinition definition) {
 		return Paths.build(Paths.STREAM_DEPLOYMENTS, definition.getName());
+	}
+
+	/**
+	 * The migration code to run against existing stream definitions that don't have module definitions set already.
+	 * The following method will update the module definitions for those stream definitions.
+	 * See https://jira.spring.io/browse/XD-2854
+	 */
+	@PostConstruct
+	private void updateModuleDefinitions() {
+		if (this.zkConnection.getClient() != null) {
+			try {
+				CuratorFramework client = this.zkConnection.getClient();
+				if (client.checkExists().forPath(Paths.STREAMS) != null) {
+					for (StreamDefinition definition : findAll()) {
+						setModuleDefinitions(client, definition);
+					}
+				}
+			}
+			catch (Exception e) {
+				logger.error("Exception migrating stream definitions. This migration is done when the existing " +
+						"stream definitions that don't have module definitions set." , e);
+			}
+		}
+	}
+
+	/**
+	 * Set the module definitions for the given stream definition.
+	 * Module definitions are obtained by parsing the stream definition.
+	 *
+	 * @param client the curator framework client
+	 * @param definition the stream definition
+	 */
+	private void setModuleDefinitions(CuratorFramework client, StreamDefinition definition) {
+		String streamName = definition.getName();
+		String path = Paths.build(Paths.STREAMS, streamName);
+		try {
+			byte[] bytes = client.getData().forPath(path);
+			if (bytes != null) {
+				Map<String, String> map = ZooKeeperUtils.bytesToMap(bytes);
+
+				// Prior to Spring XD 1.1.1/1.2, the JSON map for stream definitions
+				// contained a single key named "definition" with the value being
+				// the definition string. As of 1.1.1/1.2, the map contains an additional
+				// key "moduleDefinitions" which contains a JSON array of module
+				// information such as class name and module URL.
+				//
+				// The following will convert "old" stream definitions into the
+				// new format.
+				if (map.get(MODULE_DEFINITIONS_KEY) == null) {
+					List<ModuleDescriptor> moduleDescriptors = this.parser.parse(streamName,
+							definition.getDefinition(), definitionKind);
+					List<ModuleDefinition> moduleDefinitions = createModuleDefinitions(moduleDescriptors);
+					if (!moduleDefinitions.isEmpty()) {
+						map.put(DEFINITION_KEY, definition.getDefinition());
+						try {
+							map.put(MODULE_DEFINITIONS_KEY,
+									objectWriter.writeValueAsString(moduleDefinitions));
+							byte[] binary = ZooKeeperUtils.mapToBytes(map);
+							BackgroundPathAndBytesable<?> op = client.checkExists().forPath(path) == null
+									? client.create() : client.setData();
+							op.forPath(path, binary);
+						}
+						catch (JsonProcessingException jpe) {
+							logger.error("Exception writing module definitions " + moduleDefinitions +
+									" for the stream " + streamName, jpe);
+						}
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			logger.error("Exception when updating module definitions for the stream "
+					+ streamName, e);
+		}
 	}
 
 }

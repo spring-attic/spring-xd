e6f825801ed69aa6ee5baec656a883e07d714e71
Thomas Risberg <trisberg@pivotal.io>
2015-04-29 18:40:30 -0400

    XD-3003 Adding support for Sqoop's metastore and job command
    - job --exec requires the use of --password-file or the job will prompt for a password and execution will hang

---
+++ extensions/spring-xd-extension-sqoop/src/main/java/org/springframework/xd/sqoop/SqoopEnvironmentProvider.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.xd.sqoop;
+
+import org.springframework.batch.step.tasklet.x.EnvironmentProvider;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.Map;
+
+/**
+ * Class that sets environment variables for Sqoop
+ */
+public class SqoopEnvironmentProvider implements EnvironmentProvider {
+
+	private static final String XD_CONFIG_HOME = "xd.config.home";
+
+	ConfigurableEnvironment environment;
+
+	public SqoopEnvironmentProvider(ConfigurableEnvironment environment) {
+		this.environment = environment;
+	}
+
+	@Override
+	public void setEnvironment(Map<String, String> env) {
+		env.put("SQOOP_CONF_DIR", environment.getProperty(XD_CONFIG_HOME));
+	}
+
+}
